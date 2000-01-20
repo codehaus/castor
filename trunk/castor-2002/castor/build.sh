c2ec35536ee478e88063fa4f474f55b62411960c
arkin <arkin@b24b0d9a-6811-0410-802a-946fa971d308>
2000-01-20 22:34:38 +0000

    *** empty log message ***

--- trunk/castor-2002/castor/build.sh
+++ trunk/castor-2002/castor/build.sh
@@ -3,20 +3,19 @@
 # $Id$
 
 if [ -z "$JAVA_HOME" ] ; then
-  JAVAC=`which java`
-  if [ -z "$JAVAC" ] ; then
+  JAVA=`which java`
+  if [ -z "$JAVA" ] ; then
     echo "Cannot find JAVA. Please set your PATH."
     exit 1
   fi
-  JAVA_BIN=`dirname $JAVAC`
+  JAVA_BIN=`dirname $JAVA`
   JAVA_HOME=$JAVA_BIN/..
 fi
 
-JAVAC=$JAVA_HOME/bin/java
+JAVA=$JAVA_HOME/bin/java
 
 CLASSPATH=`echo lib/*.jar | tr ' ' ':'`:$CLASSPATH
 CLASSPATH=$JAVA_HOME/lib/tools.jar:$CLASSPATH
 
 
-$JAVAC -cp $CLASSPATH -Dant.home=lib org.apache.tools.ant.Main "$@" -buildfile src/build.xml
-
+$JAVA -classpath $CLASSPATH -Dant.home=lib org.apache.tools.ant.Main "$@" -buildfile src/build.xml
