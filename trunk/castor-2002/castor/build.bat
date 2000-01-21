3e0d783c574b1d38fd8558c6ea92601ab205aa8f
maucherat <maucherat@b24b0d9a-6811-0410-802a-946fa971d308>
2000-01-21 21:02:07 +0000

    - Fixed Windows batch files.
    Remy

--- trunk/castor-2002/castor/build.bat
+++ trunk/castor-2002/castor/build.bat
@@ -1,8 +1,7 @@
-# $Id$
 @echo off
+REM $Id$
 set JAVA=%JAVA_HOME%\bin\java
-for %%LIB in (lib\*.jar) do set CLASSPATH=%%LIB;%CLASSPATH%
+for %%i in (lib\*.jar) do set CLASSPATH=%%i;%CLASSPATH%
 set CLASSPATH=%CLASSPATH%;%JAVA_HOME%\lib\tools.jar
-
 %JAVA% -classpath %CLASSPATH% -Dant.home=lib org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 -buildfile src/build.xml
 
