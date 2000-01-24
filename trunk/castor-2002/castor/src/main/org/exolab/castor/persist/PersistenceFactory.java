ce69f5a1dd5253a78d4c772cc2616d3ba4195007
arkin <arkin@b24b0d9a-6811-0410-802a-946fa971d308>
2000-01-24 21:14:08 +0000

    First checkin. Adapted from JDO package with engine rewrite

---
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/persist/PersistenceFactory.java
@@ -0,0 +1,86 @@
+/**
+ * Redistribution and use of this software and associated documentation
+ * ("Software"), with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain copyright
+ *    statements and notices.  Redistributions must also contain a
+ *    copy of this document.
+ *
+ * 2. Redistributions in binary form must reproduce the
+ *    above copyright notice, this list of conditions and the
+ *    following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 3. The name "Exolab" must not be used to endorse or promote
+ *    products derived from this Software without prior written
+ *    permission of Exoffice Technologies.  For written permission,
+ *    please contact info@exolab.org.
+ *
+ * 4. Products derived from this Software may not be called "Exolab"
+ *    nor may "Exolab" appear in their names without prior written
+ *    permission of Exoffice Technologies. Exolab is a registered
+ *    trademark of Exoffice Technologies.
+ *
+ * 5. Due credit should be given to the Exolab Project
+ *    (http://www.exolab.org/).
+ *
+ * THIS SOFTWARE IS PROVIDED BY EXOFFICE TECHNOLOGIES AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * EXOFFICE TECHNOLOGIES OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Copyright 1999 (C) Exoffice Technologies Inc. All Rights Reserved.
+ *
+ * $Id$
+ */
+
+
+package org.exolab.castor.persist;
+
+
+import org.exolab.castor.mapping.ObjectDesc;
+import org.exolab.castor.mapping.MappingException;
+
+
+/**
+ * Factory for producing new persistence implementations. This factory
+ * is called by the cache engine to return persistence implementations
+ * for each of the object types supported by that engine. The returned
+ * implementations may be shared or one per object type. Only the
+ * cache engine requesting the implementation will ever use the
+ * returned implementation.
+ *
+ * @author <a href="arkin@exoffice.com">Assaf Arkin</a>
+ * @version $Revision$ $Date$
+ * @see Persistence
+ */
+public interface PersistenceFactory
+{
+
+
+    /**
+     * Returns a persistence implementation for the specified object
+     * type (given its descriptor) on behalf of the specified cache
+     * engine. Return null if no persistence support is available for
+     * the specified object type.
+     *
+     * @param cache The cache engine which will use this persistence
+     * @param objDesc The object descriptor
+     * @return A suiteable persistence implementation, or null
+     * @throws MappingException Indicates that the object type is not
+     *  supported by the persistence engine due to improper mapping
+     */
+    public Persistence getPersistence( CacheEngine cache, ObjectDesc objDesc )
+	throws MappingException;
+
+
+}
