5727d65ac2a0ef1df1e85b5d8815732a0bc93482
maucherat <maucherat@b24b0d9a-6811-0410-802a-946fa971d308>
2000-02-02 19:23:18 +0000

    - Fixed example.bat b.bat -> cp.bat
    Remy

--- trunk/castor-2002/castor/example.bat
+++ trunk/castor-2002/castor/example.bat
@@ -3,7 +3,7 @@ REM $Id$
 set JAVA=%JAVA_HOME%\bin\java
 set CLASSPATH=build\classes;build\examples;%CLASSPATH%
 set cp=%CLASSPATH%
-for %%i in (lib\*.jar) do call b.bat %%i
+for %%i in (lib\*.jar) do call cp.bat %%i
 set CP=%CP%;%JAVA_HOME%\lib\tools.jar
 
 %JAVA% -classpath %CP% %1.Test %2 %3 %4 %5 %6
