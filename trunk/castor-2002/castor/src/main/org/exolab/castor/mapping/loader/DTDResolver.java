eb32772d92db5b897a78a2a7aea22bd521cfae6e
arkin <arkin@b24b0d9a-6811-0410-802a-946fa971d308>
2000-03-01 21:54:47 +0000

    First checkin

---
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/mapping/loader/DTDResolver.java
@@ -0,0 +1,189 @@
+/**
+ * Redistribution and use of this software and associated documentation
+ * ("Software"), with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain copyright
+ *    statements and notices.  Redistributions must also contain a
+ *    copy of this document.
+ *
+ * 2. Redistributions in binary form must reproduce the
+ *    above copyright notice, this list of conditions and the
+ *    following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 3. The name "Exolab" must not be used to endorse or promote
+ *    products derived from this Software without prior written
+ *    permission of Exoffice Technologies.  For written permission,
+ *    please contact info@exolab.org.
+ *
+ * 4. Products derived from this Software may not be called "Exolab"
+ *    nor may "Exolab" appear in their names without prior written
+ *    permission of Exoffice Technologies. Exolab is a registered
+ *    trademark of Exoffice Technologies.
+ *
+ * 5. Due credit should be given to the Exolab Project
+ *    (http://www.exolab.org/).
+ *
+ * THIS SOFTWARE IS PROVIDED BY EXOFFICE TECHNOLOGIES AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * EXOFFICE TECHNOLOGIES OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Copyright 1999 (C) Exoffice Technologies Inc. All Rights Reserved.
+ *
+ * $Id$
+ */
+
+
+package org.exolab.castor.mapping.loader;
+
+
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.io.IOException;
+import org.xml.sax.SAXException;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+
+
+/**
+ * Entity resolved for mapping DTD/schema. Can be used as wrapper to
+ * another entity resolver. For example, if a resolver is used for
+ * external entities in the mapping file, construct a new resolver
+ * using the {@link #DTDResolver(EntityResolver)} constructor.
+ * <p>
+ * This resolver will support both the public and system identifier
+ * of either the DTD or XML schema. It will return an input stream
+ * to the DTD/schema obtained from the Castor JAR.
+ *
+ * @author <a href="mailto:arkin@exoffice.com">Assaf Arkin</a>
+ * @version $Revision$ $Date$
+ */
+public class DTDResolver
+    implements EntityResolver
+{
+
+
+    /**
+     * List of public identifiers.
+     */
+    private static final String[] PublicId = new String[] {
+        "-//EXOLAB/Castor Mapping DTD Version 1.0//EN",
+        "-//EXOLAB/Castor Mapping Schema Version 1.0//EN"
+    };
+
+    /**
+     * List of system identifiers.
+     */
+    private static final String[] SystemId = new String[] {
+        "http://castor.exolab.org/mapping.dtd",
+        "http://castor.exolab.org/mapping.xsd"
+    };
+
+    /**
+     * List of resources for the actual files.
+     */
+    private static final String[] Resource = new String[] {
+        "/org/exolab/castor/mapping/schema/mapping.dtd",
+        "/org/exolab/castor/mapping/schame/mapping.xsd"
+    };
+
+
+    public static final String NamespacePrefix =
+        "castor";
+
+    public static final String NamespaceURI =
+        "http://castor.exolab.org/mapping";
+
+
+    /**
+     * The wrapped resolver.
+     */
+    private EntityResolver _resolver;
+
+
+    /**
+     * Base URL, if known.
+     */
+    private URL            _baseUrl;
+
+
+    /**
+     * Constructs a new DTD resolver. This resolver wraps another
+     * resolver and will delegate all resolving not related to the
+     * Castor mapping files to that resolver. The wrapper resolver
+     * will typically be used for entities appearing in the actual
+     * mapping file.
+     */
+    public DTDResolver( EntityResolver resolver )
+    {
+        _resolver = resolver;
+    }
+
+
+    /**
+     * Constructs a new DTD resolver.
+     */
+    public DTDResolver()
+    {
+    }
+
+
+    public void setBaseURL( URL baseUrl )
+    {
+        _baseUrl = baseUrl;
+    }
+
+
+    public InputSource resolveEntity( String publicId, String systemId )
+        throws IOException, SAXException
+    {
+        int         i;
+        InputSource source;
+        
+        // First, resolve all the DTD/schema.
+        for ( i = 0 ; i < PublicId.length ; ++i ) {
+            if ( PublicId[ i ].equals( publicId ) || SystemId[ i ].equals( systemId ) ) {
+                return new InputSource( getClass().getResourceAsStream( Resource[ i ] ) );
+            }
+        }
+        // If a resolver was specified, use it.
+        if ( _resolver != null ) {
+            source = _resolver.resolveEntity( publicId, systemId );
+            if ( source != null )
+                return source;
+        }
+
+
+        // Can't resolve public id, but might be able to resolve relative
+        // system id, since we have a base URI.
+        if ( systemId != null && _baseUrl != null ) {
+            URL url;
+
+            try {
+                url = new URL( systemId );
+                return new InputSource( url.openStream() );
+            } catch ( MalformedURLException except ) {
+                try {
+                    url = new URL( _baseUrl, systemId );
+                    return new InputSource( url.openStream() );
+                } catch ( MalformedURLException ex2 ) { }
+            }
+            return null;
+        }
+
+        // No resolving.
+        return null;
+    }
+
+
+}
---
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/mapping/loader/FieldHandlerImpl.java
@@ -0,0 +1,324 @@
+/**
+ * Redistribution and use of this software and associated documentation
+ * ("Software"), with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain copyright
+ *    statements and notices.  Redistributions must also contain a
+ *    copy of this document.
+ *
+ * 2. Redistributions in binary form must reproduce the
+ *    above copyright notice, this list of conditions and the
+ *    following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 3. The name "Exolab" must not be used to endorse or promote
+ *    products derived from this Software without prior written
+ *    permission of Exoffice Technologies.  For written permission,
+ *    please contact info@exolab.org.
+ *
+ * 4. Products derived from this Software may not be called "Exolab"
+ *    nor may "Exolab" appear in their names without prior written
+ *    permission of Exoffice Technologies. Exolab is a registered
+ *    trademark of Exoffice Technologies.
+ *
+ * 5. Due credit should be given to the Exolab Project
+ *    (http://www.exolab.org/).
+ *
+ * THIS SOFTWARE IS PROVIDED BY EXOFFICE TECHNOLOGIES AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * EXOFFICE TECHNOLOGIES OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Copyright 1999 (C) Exoffice Technologies Inc. All Rights Reserved.
+ *
+ * $Id$
+ */
+
+
+package org.exolab.castor.mapping.loader;
+
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.InvocationTargetException;
+import org.exolab.castor.mapping.FieldHandler;
+import org.exolab.castor.mapping.TypeConvertor;
+import org.exolab.castor.mapping.MappingException;
+import org.exolab.castor.mapping.IntegrityException;
+import org.exolab.castor.util.Messages;
+
+
+/**
+ * A field handler that knows how to get/set the values of a field
+ * directly or through the get/set methods. Uses reflection.
+ *
+ * @author <a href="arkin@exoffice.com">Assaf Arkin</a>
+ * @version $Revision$ $Date$
+ */
+public final class FieldHandlerImpl
+    implements FieldHandler
+{
+
+
+    /**
+     * The Java field described and accessed through this descriptor.
+     */
+    private Field   _field;
+
+
+    /**
+     * The method used to obtain the value of this field. May be null.
+     */
+    private Method  _getMethod;
+
+
+    /**
+     * The method used to set the value of this field. May be null.
+     */
+    private Method  _setMethod;
+
+
+    /**
+     * The Java field name.
+     */
+    private String  _fieldName;
+
+
+    /**
+     * The Java field type.
+     */
+    private Class  _fieldType;
+
+
+    /**
+     * True if this field is an immutable type.
+     */
+    private boolean  _immutable;
+
+
+    /**
+     * The default value for primitive fields. Will be set if the field is null.
+     */
+    private Object   _default;
+
+
+    /**
+     * Returns true if the field is required. Required fields cannot
+     * be null on output.
+     */
+    private boolean  _required;
+
+
+    /**
+     * Convertor to apply when setting the value of the field. Converts from
+     * the value to the field type. Null if no convertor is required.
+     */
+    private TypeConvertor  _convertTo;
+
+
+    /**
+     * Convertor to apply when reading the value of the field. Converts from
+     * the field type to the return value. Null if no convertor is required.
+     */
+    private TypeConvertor  _convertFrom;
+
+
+    /**
+     * Construct a new field handler for the specified field. The field must
+     * be public, and may not be static or transient. The field name is
+     * determined from the Java field, the type from the type information.
+     *
+     * @param field The field being described
+     * @param typeInfo Type information
+     * @throws MappingException If the field is not public, is static or
+     *    transient
+     */
+    public FieldHandlerImpl( Field field, TypeInfo typeInfo )
+        throws MappingException
+    {
+        if ( field.getModifiers() != Modifier.PUBLIC &&
+             field.getModifiers() != ( Modifier.PUBLIC | Modifier.VOLATILE ) )
+            throw new MappingException( "mapping.fieldNotAccessible", field.getName(),
+                                        field.getDeclaringClass().getName() );
+        _field = field;
+        _fieldName = field.getName();
+        _fieldType = Types.typeFromPrimitive( typeInfo.getFieldType() );
+        _immutable = typeInfo.isImmutable();
+        _required = typeInfo.isRequired();
+        _default = typeInfo.getDefaultValue();
+        _convertTo = typeInfo.getConvertorTo();
+        _convertFrom = typeInfo.getConvertorFrom();
+    }
+
+
+    /**
+     * Construct a new field handler for the specified field that
+     * is accessed through the accessor methods (get/set). The accessor
+     * methods must be public and not static. The field name is
+     * required for descriptive purposes. The field type must match
+     * the return value of the get method and the single parameter of
+     * the set method. Either get or set methods are optional.
+     *
+     * @param fieldName The field being described
+     * @param getMethod The method used to retrieve the field value,
+     *  must accept no parameters and have a return type castable to
+     *  the field type
+     * @param setMethod The method used to set the field value, must
+     *  accept a single paramater that is castable to the field type
+     * @param typeInfo Type information
+     * @throws MappingException If the get or set method are not public,
+     *   are static, or do not specify the proper types
+     *
+     */
+    public FieldHandlerImpl( String fieldName, Method getMethod, Method setMethod, TypeInfo typeInfo )
+        throws MappingException
+    {
+        if ( fieldName == null )
+            throw new IllegalArgumentException( "Argument 'fieldName' is null" );
+        _fieldName = fieldName;
+        if ( getMethod == null && setMethod == null )
+            throw new IllegalArgumentException( "Both arguments 'getMethod' and 'setMethod' are null" );
+        
+        if ( setMethod != null ) {
+            if ( ( setMethod.getModifiers() & Modifier.PUBLIC ) == 0 ||
+                 ( setMethod.getModifiers() & Modifier.STATIC ) != 0 )
+                throw new MappingException( "mapping.accessorNotAccessible",
+                                            setMethod, setMethod.getDeclaringClass().getName() );
+            _setMethod = setMethod;
+        }
+
+        if ( getMethod != null ) {
+            if ( ( getMethod.getModifiers() & Modifier.PUBLIC ) == 0 ||
+                 ( getMethod.getModifiers() & Modifier.STATIC ) != 0 ) 
+                throw new MappingException( "mapping.accessorNotAccessible",
+                                            getMethod, getMethod.getDeclaringClass().getName() );
+            _getMethod = getMethod;
+        }
+
+        _fieldType = Types.typeFromPrimitive( typeInfo.getFieldType() );
+        _immutable = typeInfo.isImmutable();
+        _required = typeInfo.isRequired();
+        _default = typeInfo.getDefaultValue();
+        _convertTo = typeInfo.getConvertorTo();
+        _convertFrom = typeInfo.getConvertorFrom();
+    }
+
+
+    public Object getValue( Object object )
+    {
+        Object value;
+
+        try {
+            // Get the field value
+            if ( _field != null )
+                value = _field.get( object );
+            else
+                value = _getMethod.invoke( object, null );
+        } catch ( IllegalAccessException except ) {
+            // This should never happen
+            throw new IllegalStateException( Messages.format( "mapping.schemaChangeNoAccess", toString() ) );
+        } catch ( InvocationTargetException except ) {
+            // This should never happen
+            throw new IllegalStateException( Messages.format( "mapping.schemaChangeInvocation",
+                                                              toString(), except.getMessage() ) );
+        }
+
+        // If there is a convertor, apply it
+        if ( _convertFrom == null )
+            return value;
+        try {
+            return _convertFrom.convert( value );
+        } catch ( ClassCastException except ) {
+            throw new IllegalArgumentException( Messages.format( "mapping.wrongConvertor",  value.getClass().getName() ) );
+        }
+    }
+    
+
+    public void setValue( Object object, Object value )
+    {
+        // If there is a default value, use it for a null field.
+        // Otherwise, if there is a convertor, apply conversion.
+        if ( value == null && _default != null )
+            value = _default;
+        else if ( _convertTo != null ) {
+            try {
+                value = _convertTo.convert( value );
+            } catch ( ClassCastException except ) {
+                throw new IllegalArgumentException( Messages.format( "mapping.wrongConvertor", value.getClass().getName() ) );
+            }
+        }
+
+        try {
+            if ( _field != null )
+                _field.set( object, value );
+            else
+                _setMethod.invoke( object, new Object[] { value } );
+            // If the field has no set method, ignore it.
+            // If this is a problem, identity it someplace else.
+        } catch ( IllegalArgumentException except ) {
+            // Graceful way of dealing with unwrapping exception
+            if ( value == null )
+                throw new IllegalArgumentException( Messages.format( "mapping.typeConversionNull", toString() ) );
+            else
+                throw new IllegalArgumentException( Messages.format( "mapping.typeConversion",
+                                                                     toString(), value.getClass().getName() ) );
+        } catch ( IllegalAccessException except ) {
+            // This should never happen
+            throw new IllegalStateException( Messages.format( "mapping.schemaChangeNoAccess", toString() ) );
+        } catch ( InvocationTargetException except ) {
+            // This should never happen
+            throw new IllegalStateException( Messages.format( "mapping.schemaChangeInvocation",
+                                                              toString(), except.getMessage() ) );
+        }
+    }
+
+
+    public void checkIntegrity( Object object )
+        throws IntegrityException
+    {
+        try {
+            if ( _required ) {
+                if ( ( _field != null && _field.get( object ) == null ) ||
+                     ( _field == null && _getMethod.invoke( object, null ) == null ) )
+                throw new IntegrityException( "mapping.requiredField",
+                                              object.getClass().getName(), _fieldName );
+            }
+        } catch ( IllegalAccessException except ) {
+            // This should never happen
+            throw new IllegalStateException( Messages.format( "mapping.schemaChangeNoAccess", toString() ) );
+        } catch ( InvocationTargetException except ) {
+            // This should never happen
+            throw new IllegalStateException( Messages.format( "mapping.schemaChangeInvocation",
+                                                              toString(), except.getMessage() ) );
+        }
+    }
+
+
+    public Object newInstance( Object object )
+        throws IllegalStateException
+    {
+        if ( _immutable )
+            throw new IllegalStateException( Messages.format( "mapping.classNotConstructable", _fieldType ) );
+        // XXX This is the place to call the create method
+        return Types.newInstance( _fieldType );
+    }
+
+
+    public String toString()
+    {
+        return _fieldName + "(" + _fieldType.getName() + ")";
+    }
+    
+
+}
+
