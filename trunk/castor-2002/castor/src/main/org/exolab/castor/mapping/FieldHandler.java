0fdb9477f7253cc761428d2e0ff0dec4a4334667
arkin <arkin@b24b0d9a-6811-0410-802a-946fa971d308>
2000-03-03 23:54:03 +0000

    Updated docs/comments

--- trunk/castor-2002/castor/src/main/org/exolab/castor/mapping/FieldHandler.java
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/mapping/FieldHandler.java
@@ -53,7 +53,7 @@ package org.exolab.castor.mapping;
  *
  * @author <a href="arkin@exoffice.com">Assaf Arkin</a>
  * @version $Revision$ $Date$
- * @see FieldDesc
+ * @see FieldDescriptor
  */
 public interface FieldHandler
 {
