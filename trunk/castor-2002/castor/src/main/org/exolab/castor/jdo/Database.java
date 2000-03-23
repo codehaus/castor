ce3adcdad2847ccbbc6bcb5dff79aee9c187050d
arkin <arkin@b24b0d9a-6811-0410-802a-946fa971d308>
2000-03-23 03:08:10 +0000

    Moved transaction control to Database

--- trunk/castor-2002/castor/src/main/org/exolab/castor/jdo/Database.java
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/jdo/Database.java
@@ -113,5 +113,75 @@ public interface Database
                PersistenceException;
 
 
+
+    /**
+     * Start a transaction.
+     * Calling <code>begin</code> multiple times on the same transaction object,
+     * without an intervening call to <code>commit</code> or <code>abort</code>,
+     * causes the exception <code>TransactionInProgressException</code> to be thrown
+     * on the second and subsequent calls. Operations executed before a transaction
+     * has been opened, or before reopening after a transaction is aborted or committed,
+     * have undefined results;
+     * these may throw a <code>TransactionNotInProgressException</code> exception.
+     */
+    public void begin();
+
+
+    /**
+     * Commit and close the transaction.
+     * Calling <code>commit</code> commits to the database all persistent object
+     * modifications within the transaction and releases any locks held by the transaction.
+     * A persistent object modification is an update of any field of an existing
+     * persistent object, or an update or creation of a new named object in the database.
+     * If a persistent object modification results in a reference from an existing
+     * persistent object to a transient object, the transient object is moved to the
+     * database, and all references to it updated accordingly. Note that the act of
+     * moving a transient object to the database may create still more persistent
+     * references to transient objects, so its referents must be examined and moved as well.
+     * This process continues until the database contains no references to transient objects,
+     * a condition that is guaranteed as part of transaction commit.
+     * Committing a transaction does not remove from memory transient objects created
+     * during the transaction
+     */
+    public void commit()
+        throws TransactionNotInProgressException, TransactionAbortedException, PersistenceException;
+
+
+    /**
+     * Abort and close the transaction.
+     * Calling abort abandons all persistent object modifications and releases the
+     * associated locks.
+     * Aborting a transaction does not restore the state of modified transient objects
+     */
+    public void abort()
+        throws TransactionNotInProgressException, PersistenceException;
+
+
+    /**
+     * Commit the transaction, but reopen the transaction, retaining all locks.
+     * Calling <code>checkpoint</code> commits persistent object modifications made
+     * within the transaction since the last checkpoint to the database.
+     * The transaction retains all locks it held on those objects at the time the
+     * checkpoint was invoked.
+     */
+    public void checkpoint()
+        throws TransactionNotInProgressException, TransactionAbortedException, PersistenceException;
+
+
+    /**
+     * Upgrade the lock on the given object to the given lock mode.
+     * The call has no effect if the object's current lock is already at or above
+     * that level of lock mode.
+     *
+     * @param obj The object to acquire a lock on.
+     * @param lockMode The lock mode to acquire. The lock modes are <code>READ</code>,
+     * <code>UPGRADE</code>, and <code>WRITE</code>.
+     * @throws LockNotGrantedException Is thrown if the given lock mode could not be acquired.
+     */
+    public void lock( Object obj )
+        throws LockNotGrantedException, PersistenceException;
+
+
+
 }
 
