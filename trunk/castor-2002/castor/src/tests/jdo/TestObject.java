a0b8e32312f31042ae63057061795dc7a13354c6
arkin <arkin@b24b0d9a-6811-0410-802a-946fa971d308>
2000-03-23 03:19:33 +0000

    *** empty log message ***

---
+++ trunk/castor-2002/castor/src/tests/jdo/TestObject.java
@@ -0,0 +1,26 @@
+package jdo;
+
+
+public class TestObject
+{
+
+
+    public int  id;
+
+
+    public String name;
+
+
+    static final int       DefaultId = 3;
+
+
+    static final String    DefaultName = "three";
+
+
+    public String toString()
+    {
+        return id + "/" + name;
+    }
+
+
+}
