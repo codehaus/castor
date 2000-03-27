709c1ac6a22f6365f2ef0af3429be767ea1241b8
arkin <arkin@b24b0d9a-6811-0410-802a-946fa971d308>
2000-03-27 23:22:33 +0000

    Comments, added load and isPersistent methods

--- trunk/castor-2002/castor/src/main/org/exolab/castor/jdo/Database.java
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/jdo/Database.java
@@ -1,43 +1,159 @@
+/**
+ * Redistribution and use of this software and associated documentation
+ * ("Software"), with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain copyright
+ *    statements and notices.  Redistributions must also contain a
+ *    copy of this document.
+ *
+ * 2. Redistributions in binary form must reproduce the
+ *    above copyright notice, this list of conditions and the
+ *    following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 3. The name "Exolab" must not be used to endorse or promote
+ *    products derived from this Software without prior written
+ *    permission of Exoffice Technologies.  For written permission,
+ *    please contact info@exolab.org.
+ *
+ * 4. Products derived from this Software may not be called "Exolab"
+ *    nor may "Exolab" appear in their names without prior written
+ *    permission of Exoffice Technologies. Exolab is a registered
+ *    trademark of Exoffice Technologies.
+ *
+ * 5. Due credit should be given to the Exolab Project
+ *    (http://www.exolab.org/).
+ *
+ * THIS SOFTWARE IS PROVIDED BY EXOFFICE TECHNOLOGIES AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * EXOFFICE TECHNOLOGIES OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Copyright 1999 (C) Exoffice Technologies Inc. All Rights Reserved.
+ *
+ * $Id$
+ */
+
 
 package org.exolab.castor.jdo;
 
 
+
+
 /**
+ * An open connection to the database. This object represents an open
+ * connection to the database that can be used to perform transactional
+ * operations on the database.
+ * <p>
+ * Database operations can only be performed in the context of a
+ * transaction. Client applications should begin and commit a transaction
+ * using the {@link #begin} and {@link #commit} methods. Server
+ * applications should use implicit transaction demaraction by the
+ * container or explicit transaction demarcation using <tt>javax.transaction.UserTransaction</tt>.
+ * <p>
+ * All objects queried and created during a transaction are persistent.
+ * Changes to persistent objects will be stored in the database when
+ * the transaction commits. Changes will not be stored if the transaction
+ * is rolled back or fails to commit.
+ * <p>
+ * Once the transaction has committed or rolled back, all persistent
+ * objects become transient. Opening a new transaction does not make
+ * these objects persistent.
+ * <p>
+ * For example:
+ * <pre>
+ * Database    db;
+ * OQLQuery    oql;
+ * Product     prod;
+ * Enumeration enum;
  *
+ * <font color="red">// Open a database and start a transaction</font>
+ * db = jdo.getDatabase();
+ * db.begin();
+ * <font color="red">// Select all the products in a given group</font>
+ * oql = db.getOQLQuery( "SELECT p FROM Product p WHERE group=$");
+ * oql.bind( groupId );
+ * enum = oql.execute();
+ * while ( enum.hasMoreElements() ) {
+ *   <font color="red">// A 25% mark down for each product and mark as sale</font>
+ *   prod = (Product) enum.nextElement();
+ *   prod.price = prod.price * 0.75;
+ *   prod.onSale = true;
+ * }
+ * <font color="red">// Commit all changes, close the database</font>
+ * db.commit();
+ * db.close();
+ * </pre>
+ *
+ * @author <a href="arkin@exoffice.com">Assaf Arkin</a>
+ * @version $Revision$ $Date$
+ * @see DataObjects#getDatabase
+ * @see OQLQuery
  */
 public interface Database
 {
 
 
     /**
-     * Returns a new OQL query.
+     * Creates an OQL query with no statement. {@link OQLQuery#create}
+     * must be called before the query can be executed.
+     *
+     * @return An OQL query
      */
     public OQLQuery getOQLQuery();
 
 
     /**
-     * Returns a new OQL query using the specified statement.
+     * Creates an OQL query from the supplied statement.
+     *
+     * @param query An OQL query statement
+     * @return An OQL query
+     * @throws QueryException The query syntax is invalid
      */
     public OQLQuery getOQLQuery( String oql )
         throws QueryException;
 
 
     /**
-     * Closes the database.
+     * Load an object of the specified type and given identity.
+     * Once loaded the object is persistent. Calling this method with
+     * the same identity in the same transaction will return the same
+     * object. This method is equivalent to a query that returns a
+     * single object.
      *
-     * @throws ODMGException Unable to close the database
+     * @param type The object's type
+     * @param identity The object's identity
+     * @throws ObjectNotFoundException No object of the given type and
+     *  identity was found in persistent storage
+     * @throws LockNotGrantedException Timeout or deadlock occured
+     *  attempting to acquire a lock on the object
+     * @throws TransactionNotInProgressException Method called while
+     *   transaction is not in progress
+     * @throws PersistenceException An error reported by the
+     *  persistence engine
      */
-    public void close()
-        throws PersistenceException;
+    public Object load( Class type, Object identity )
+        throws ObjectNotFoundException, LockNotGrantedException,
+               TransactionNotInProgressException, PersistenceException;
 
 
     /**
-     * Creates a new object in persistent storage and returns the.
-     * The object will be persisted only if the transaction commits.
-     * If the object has an identity then duplicate identity check
-     * happens in this method, if the identity is null,  duplicate
-     * identity check occurs when the transaction completes and the
-     * object is not visible in this transaction.
+     * Creates a new object in persistent storage. The object will be
+     * persisted only if the transaction commits. If the object has an
+     * identity then duplicate identity check happens in this method,
+     * and the object is visible to queries in this transaction.
+     * If the identity is null, duplicate identity check occurs when
+     * the transaction completes and the object is not visible to
+     * queries until the transaction commits.
      *
      * @param object The object to create
      * @throws TransactionNotInProgressException Method called while
@@ -57,9 +173,9 @@ public interface Database
     /**
      * Removes the object from persistent storage. The deletion will
      * take effect only if the transaction is committed, but the
-     * object is no longer viewable for the current transaction and
-     * locks for access from other transactions will block until this
-     * transaction completes.
+     * object is no longer visible to queries in the current transaction
+     * and locks for access from other transactions will block until
+     * this transaction completes.
      *
      * @param object The object to remove
      * @throws TransactionNotInProgressException Method called while
@@ -67,67 +183,13 @@ public interface Database
      * @throws ObjectNotPersistentException The object has not been
      *  queried or created in this transaction
      * @throws LockNotGrantedException Timeout or deadlock occured
-     *  attempting to acquire lock on object
+     *  attempting to acquire a lock on the object
      * @throws PersistenceException An error reported by the
      *  persistence engine
      */
     public void remove( Object object )
         throws ObjectNotPersistentException, LockNotGrantedException, 
                TransactionNotInProgressException, PersistenceException;
-
-
-    /**
-     * Start a transaction.
-     * Calling <code>begin</code> multiple times on the same transaction object,
-     * without an intervening call to <code>commit</code> or <code>abort</code>,
-     * causes the exception <code>TransactionInProgressException</code> to be thrown
-     * on the second and subsequent calls. Operations executed before a transaction
-     * has been opened, or before reopening after a transaction is aborted or committed,
-     * have undefined results;
-     * these may throw a <code>TransactionNotInProgressException</code> exception.
-     */
-    public void begin();
-
-
-    /**
-     * Commit and close the transaction.
-     * Calling <code>commit</code> commits to the database all persistent object
-     * modifications within the transaction and releases any locks held by the transaction.
-     * A persistent object modification is an update of any field of an existing
-     * persistent object, or an update or creation of a new named object in the database.
-     * If a persistent object modification results in a reference from an existing
-     * persistent object to a transient object, the transient object is moved to the
-     * database, and all references to it updated accordingly. Note that the act of
-     * moving a transient object to the database may create still more persistent
-     * references to transient objects, so its referents must be examined and moved as well.
-     * This process continues until the database contains no references to transient objects,
-     * a condition that is guaranteed as part of transaction commit.
-     * Committing a transaction does not remove from memory transient objects created
-     * during the transaction
-     */
-    public void commit()
-        throws TransactionNotInProgressException, TransactionAbortedException, PersistenceException;
-
-
-    /**
-     * Abort and close the transaction.
-     * Calling abort abandons all persistent object modifications and releases the
-     * associated locks.
-     * Aborting a transaction does not restore the state of modified transient objects
-     */
-    public void rollback()
-        throws TransactionNotInProgressException, PersistenceException;
-
-
-    /**
-     * Commit the transaction, but reopen the transaction, retaining all locks.
-     * Calling <code>checkpoint</code> commits persistent object modifications made
-     * within the transaction since the last checkpoint to the database.
-     * The transaction retains all locks it held on those objects at the time the
-     * checkpoint was invoked.
-     */
-    public void checkpoint()
-        throws TransactionNotInProgressException, TransactionAbortedException, PersistenceException;
 
 
     /**
@@ -149,14 +211,13 @@ public interface Database
      * @throws ObjectNotPersistentException The object has not been
      *  queried or created in this transaction
      * @throws LockNotGrantedException Timeout or deadlock occured
-     *  attempting to acquire lock on object
+     *  attempting to acquire a lock on the object
      * @throws PersistenceException An error reported by the
      *  persistence engine
      */
     public void lock( Object object )
         throws LockNotGrantedException, ObjectNotPersistentException,
                TransactionNotInProgressException,  PersistenceException;
-
 
 
     /**
@@ -172,17 +233,79 @@ public interface Database
 
 
     /**
-     * Makes a transient object durable in the database. Must be
-     * executed in the context of an open transaction.
-     * <p>
-     * If the transaction in which this method is executed commits,
-     * then the object is made durable. If the transaction aborts,
-     * then this operation is considered not to have been executed,
-     * and the target object is again transient.
+     * Begin a new transaction. A transaction must be open in order
+     * to query and persist objects.
      *
-     * @param object The object to make persistent
-     * @throws ClassNotPersistenceCapableException The object cannot
-     *  be persisted due to the type of the object
+     * @throws PersistenceException A transaction is already open on
+     *  this database, or an error reported by the persistence engine
+     */
+    public void begin()
+        throws PersistenceException;
+
+
+    /**
+     * Commits and closes the transaction. All changes made to persistent
+     * objects during the transaction are made persistent, objects
+     * created during the transaction are made durable, and objects
+     * removed during the transaction are removed from the database.
+     * <p>
+     * If the transaction cannot commit, the entire transaction rolls
+     * back and a {@link TransactionAbortedException} exception is
+     * thrown.
+     * <p>
+     * After this method returns, the transaction is closed and all
+     * persistent objects are transient. Using {@link #begin} to open a
+     * new transaction will not restore objects to their persistent
+     * stage.
+     *
+     * @throws TransactionNotInProgressException Method called while
+     *  transaction is not in progress
+     * @throws TransactionAbortedException The transaction cannot
+     *  commit and has been rolled back
+     */
+    public void commit()
+        throws TransactionNotInProgressException, TransactionAbortedException;
+
+
+    /**
+     * Rolls back and closes the transaction. All changes made to
+     * persistent objects during the transaction are lost, objects
+     * created during the transaction are not made durable and objects
+     * removed during the transaction continue to exist.
+     *
+     * @throws TransactionNotInProgressException Method called while
+     *  transaction is not in progress
+     */
+    public void rollback()
+        throws TransactionNotInProgressException;
+
+
+    /**
+     * Commits the transaction but keeps the transaction open. This
+     * has the same effect as calling {@link #commit}, but persistent
+     * objects remain persistent and all locks are retained.
+     *
+     * @throws TransactionNotInProgressException Method called while
+     *  transaction is not in progress
+     * @throws TransactionAbortedException The transaction cannot
+     *  commit and has been rolled back
+     */
+    public void checkpoint()
+        throws TransactionNotInProgressException, TransactionAbortedException;
+
+
+    /**
+     * Closes the database. If a transaction is in progress the
+     * transaction will be rolled back and an exception thrown.
+     *
+     * @throws PersistenceException An error occured while
+     *  attempting to close the database
+     */
+    public void close()
+        throws PersistenceException;
+
+
+    /**
      * @deprecated See {@link #create}
      */
     public void makePersistent( Object object )
@@ -191,19 +314,6 @@ public interface Database
 
 
     /**
-     * Deletes an object from the database. Must be executed in the
-     * context of an open transaction.
-     * <p>
-     * If the object is not persistent, then {@link ObjectNotPersistent}
-     * is thrown. If the transaction in which this method is executed
-     * commits, then the object is removed from the database. If the
-     * transaction aborts, then this operation is considered not to
-     * have been executed, and the target object is again in the
-     * database.
-     *
-     * @param object The object to delete
-     * @throws ObjectNotPersistent The object does not exist in the
-     *  database
      * @deprecated See {@link #remove}
      */
     public void deletePersistent( Object object )
