4f6c2196e45ced0216402e6618b136e772c7e61e
nobody <nobody@b24b0d9a-6811-0410-802a-946fa971d308>
2000-05-02 21:14:03 +0000

    This commit was manufactured by cvs2svn to create tag 'DEV0_8_3'.

---
+++ tags/DEV0_8_3/castor-2002/castor/src/tests/jdo/Join.java
@@ -0,0 +1,147 @@
+/**
+ * Redistribution and use of this software and associated documentation
+ * ("Software"), with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain copyright
+ *    statements and notices.  Redistributions must also contain a
+ *    copy of this document.
+ *
+ * 2. Redistributions in binary form must reproduce the
+ *    above copyright notice, this list of conditions and the
+ *    following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 3. The name "Exolab" must not be used to endorse or promote
+ *    products derived from this Software without prior written
+ *    permission of Exoffice Technologies.  For written permission,
+ *    please contact info@exolab.org.
+ *
+ * 4. Products derived from this Software may not be called "Exolab"
+ *    nor may "Exolab" appear in their names without prior written
+ *    permission of Exoffice Technologies. Exolab is a registered
+ *    trademark of Exoffice Technologies.
+ *
+ * 5. Due credit should be given to the Exolab Project
+ *    (http://www.exolab.org/).
+ *
+ * THIS SOFTWARE IS PROVIDED BY EXOFFICE TECHNOLOGIES AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * EXOFFICE TECHNOLOGIES OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Copyright 1999 (C) Exoffice Technologies Inc. All Rights Reserved.
+ *
+ * $Id$
+ */
+
+
+package jdo;
+
+
+import java.io.IOException;
+import java.util.Enumeration;
+import org.exolab.castor.jdo.DataObjects;
+import org.exolab.castor.jdo.Database;
+import org.exolab.castor.jdo.OQLQuery;
+import org.exolab.castor.jdo.PersistenceException;
+import org.exolab.castor.jdo.DuplicateIdentityException;
+import org.exolab.castor.jdo.TransactionAbortedException;
+import org.exolab.jtf.CWVerboseStream;
+import org.exolab.jtf.CWTestCase;
+import org.exolab.jtf.CWTestCategory;
+import org.exolab.exceptions.CWClassConstructorException;
+
+
+/**
+ */
+public class Join
+    extends CWTestCase
+{
+
+
+    private JDOCategory    _category;
+
+
+    public Join( String name, String description, CWTestCategory category )
+        throws CWClassConstructorException
+    {
+        super( name, description );
+        _category = (JDOCategory) category;
+    }
+
+
+    public void preExecute()
+    {
+        super.preExecute();
+    }
+
+
+    public void postExecute()
+    {
+        super.postExecute();
+    }
+
+
+    public boolean run( CWVerboseStream stream )
+    {
+        boolean result = true;
+        Database db;
+
+        try {
+            OQLQuery      oql;
+            OQLQuery      groupOql;
+            TestMaster    master;
+            TestGroup     group;
+            Enumeration   enum;
+            
+            db = _category.getDatabase( stream.verbose() );
+            db.begin();
+            oql = db.getOQLQuery( "SELECT master FROM jdo.TestMaster master WHERE id = $1" );
+            oql.bind( TestMaster.DefaultId );
+            enum = oql.execute();
+            while ( enum.hasMoreElements() ) {
+                master = (TestMaster) enum.nextElement();
+                db.remove( master );
+                stream.writeVerbose( "Deleting old master" );
+            }
+            groupOql = db.getOQLQuery( "SELECT group FROM jdo.TestGroup group WHERE id = $1" );
+            groupOql.bind( TestGroup.DefaultId );
+            enum = groupOql.execute();
+            if ( enum.hasMoreElements() ) {
+                group = (TestGroup) enum.nextElement();
+                stream.writeVerbose( "Using existing group: " + group );
+            } else {
+                group = new TestGroup();
+                db.create( group );
+                stream.writeVerbose( "Creating new group: " + group );
+            }
+            db.commit();
+
+            db.begin();
+            groupOql.bind( TestGroup.DefaultId );
+            group = (TestGroup) groupOql.execute().nextElement();
+            master = new TestMaster();
+            master.setGroup( group );
+            db.create( master );
+            db.commit();
+
+            db.close();
+        } catch ( Exception except ) {
+            stream.writeVerbose( "Error: " + except );
+            except.printStackTrace();
+            result = false;
+        }
+        return result;
+    }
+
+
+}
---
+++ tags/DEV0_8_3/castor-2002/castor/src/tests/jdo/sybase.sql
@@ -0,0 +1,59 @@
+create table test_table (
+  id      int          not null,
+  first   varchar(200) not null,
+  second  varchar(200) null
+)
+go
+
+create unique index test_table_pk on test_table ( id )
+go
+
+create table test_master (
+  id       int          not null,
+  value    varchar(200) not null,
+  group_id int          not null
+)
+go
+
+create unique index test_master_pk on test_master ( id )
+go
+
+create table test_detail (
+  detail_id  int          not null,
+  master_id  int          not null,
+  value      varchar(200) not null
+)
+go
+
+create unique index test_detail_pk on test_detail ( detail_id )
+go
+
+create table test_group (
+  id     int          not null,
+  value  varchar(200) not null
+)
+go
+
+create unique index test_group_pk on test_group ( id )
+go
+
+create table test_many (
+  id     int          not null,
+  value  varchar(200) not null
+)
+go
+
+create unique index test_many_pk on test_many ( id )
+go
+
+create table test_many_rel (
+  master_id  int not null,
+  many_id    int not null
+)
+go
+
+create unique index test_many_rel_pk on test_many_rel ( master_id, many_id )
+go
+
+
+
