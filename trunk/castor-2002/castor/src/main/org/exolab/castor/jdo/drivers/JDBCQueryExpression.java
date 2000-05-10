3627b53d6b92e2672cf17f6c824857cfd1559a79
arkin <arkin@b24b0d9a-6811-0410-802a-946fa971d308>
2000-05-10 18:17:25 +0000

    Fix to inner join

--- trunk/castor-2002/castor/src/main/org/exolab/castor/jdo/drivers/JDBCQueryExpression.java
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/jdo/drivers/JDBCQueryExpression.java
@@ -240,7 +240,8 @@ public class JDBCQueryExpression
                 for ( int j = 0 ; j < join.leftColumns.length ; ++j ) {
                     if ( j > 0 )
                         sql.append( JDBCSyntax.And );
-                    sql.append( join.leftColumns[ j ] ).append( OpEquals ).append( join.rightColumns[ j ] );
+                    sql.append( join.leftTable ).append( "." ).append( join.leftColumns[ j ] ).append( OpEquals );
+                    sql.append( join.rightTable ).append( "." ).append( join.rightColumns[ j ] );
                 }
             }
         }
