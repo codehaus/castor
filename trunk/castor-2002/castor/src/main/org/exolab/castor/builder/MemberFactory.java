bad4da81dc42fde0916e056786e217eb057d4f1c
kvisco <kvisco@b24b0d9a-6811-0410-802a-946fa971d308>
2000-06-02 00:42:15 +0000

    reverted back some changes....I need a new approach

--- trunk/castor-2002/castor/src/main/org/exolab/castor/builder/MemberFactory.java
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/builder/MemberFactory.java
@@ -157,7 +157,7 @@ public class MemberFactory {
             //-- This is NOT clean...we need a different approach
             //-- here...
             //-- modify package name if necessary
-            if (enumeration || (datatype.getSource() != null)) {
+            if (enumeration) {
                 JClass jClass = (JClass) xsType.getJType();
                 String packageName = jClass.getPackageName();
                 if ((packageName != null) && (packageName.length() > 0))
@@ -252,7 +252,8 @@ public class MemberFactory {
             //-- This is NOT clean...we need a different approach
             //-- here...
             //-- modify package name if necessary
-            if (datatype.getSource() != null) {
+            if (datatype.hasFacet(Facet.ENUMERATION)) {
+                
                 JClass jClass = (JClass) xsType.getJType();
                 String packageName = jClass.getPackageName();
                 if ((packageName != null) && (packageName.length() > 0))
