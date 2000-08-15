815521eca535a8af6442181193902dadaa7ee690
maucherat <maucherat@b24b0d9a-6811-0410-802a-946fa971d308>
2000-08-15 22:42:46 +0000

    Java 2 types compatibility (submitted by Patrick Higgins)

--- trunk/castor-2002/castor/src/main/org/exolab/castor/builder/CollectionInfoJ2.java
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/builder/CollectionInfoJ2.java
@@ -104,19 +104,132 @@ public class CollectionInfoJ2 extends CollectionInfo {
     } //-- createAddMethod
     
                     
+    /**
+     * Creates implementation of Enumerate method.
+     **/
+    public void createEnumerateMethod(JMethod method) {
+        
+        JSourceCode jsc = method.getSourceCode();
+        
+        jsc.add("return new org.exolab.castor.util.IteratorEnumeration(");
+        jsc.append(getName());
+        jsc.append(".iterator());");
+        
+    } //-- createEnumerateMethod
+
     /** 
-     * Creates implementation of Enumeration get() method.
+     * Creates implementation of object[] get() method.
     **/
     public void createGetMethod(JMethod method) {
 
         JSourceCode jsc = method.getSourceCode();
         JType jType = method.getReturnType();
-                    
-        jsc.add("return new org.exolab.castor.util.IteratorEnumeration( ");
+        
+        jsc.add("int size = ");
         jsc.append(getName());
-        jsc.append(".iterator() );");
+        jsc.append(".size();");
+        
+        String variableName = getName()+".get(index)";
+        
+        JType compType = jType.getComponentType();
+        
+        jsc.add(compType.toString());
+        jsc.append("[] mArray = new ");
+        jsc.append(compType.getLocalName());
+        jsc.append("[size]");
+        //-- if component is an array, we must add [] after setting
+        //-- size
+        if (compType.isArray()) jsc.append("[]");
+        jsc.append(";");
+        
+        jsc.add("for (int index = 0; index < size; index++) {");
+        jsc.indent();
+        jsc.add("mArray[index] = ");
+        if (getContentType().getType() == XSType.CLASS) {
+            jsc.append("(");
+            jsc.append(jType.getLocalName());
+            jsc.append(") ");
+            jsc.append(variableName);
+        }
+        else {
+            jsc.append(getContentType()
+                       .createFromJavaObjectCode(variableName));
+        }
+        jsc.append(";");
+        jsc.unindent();
+        jsc.add("}");
+        jsc.add("return mArray;");
     } //-- createGetMethod
 
+    /** 
+     * Creates implementation of the get(index) method.
+    **/
+    public void createGetByIndexMethod(JMethod method) {
+        
+        JSourceCode jsc = method.getSourceCode();
+        JType jType = method.getReturnType();
+        
+        jsc.add("//-- check bounds for index");
+        jsc.add("if ((index < 0) || (index > ");
+        jsc.append(getName());
+        jsc.append(".size())) {");
+        jsc.indent();
+        jsc.add("throw new IndexOutOfBoundsException();");
+        jsc.unindent();
+        jsc.add("}");
+        
+        jsc.add("");
+        jsc.add("return ");
+        
+        String variableName = getName()+".get(index)";
+        
+        if (getContentType().getType() == XSType.CLASS) {
+            jsc.append("(");
+            jsc.append(jType.toString());
+            jsc.append(") ");
+            jsc.append(variableName);
+        }
+        else {
+            jsc.append(getContentType().createFromJavaObjectCode(variableName));
+        }
+        jsc.append(";");
+    } //-- createGetByIndex
+
+    /** 
+     * Creates implementation of set method.
+    **/
+    public void createSetMethod(JMethod method) {
+
+        JSourceCode jsc = method.getSourceCode();
+        
+        jsc.add("//-- check bounds for index");
+        jsc.add("if ((index < 0) || (index > ");
+        jsc.append(getName());
+        jsc.append(".size())) {");
+        jsc.indent();
+        jsc.add("throw new IndexOutOfBoundsException();");
+        jsc.unindent();
+        jsc.add("}");
+        
+        int maxSize = getXSList().getMaximumSize();
+        if (maxSize != 0) {
+            jsc.add("if (!(");
+            jsc.append(getName());
+            jsc.append(".size() < ");
+            jsc.append(Integer.toString(maxSize));
+            jsc.append(")) {");
+            jsc.indent();
+            jsc.add("throw new IndexOutOfBoundsException();");
+            jsc.unindent();
+            jsc.add("}");
+        }
+        jsc.add(getName());
+        jsc.append(".set(index, ");
+        jsc.append(getContentType().createToJavaObjectCode(getContentName()));
+        jsc.append(");");
+        
+    } //-- createSetMethod
+    
     /** 
      * Creates implementation of remove(Object) method.
     **/
@@ -129,6 +242,58 @@ public class CollectionInfoJ2 extends CollectionInfo {
         jsc.append(getContentType().createToJavaObjectCode(getContentName()));
         jsc.append(");");
     } //-- createRemoveMethod
+
+    /** 
+     * Creates implementation of remove(Object) method.
+    **/
+    public void createRemoveByObjectMethod(JMethod method) {
+        
+        JSourceCode jsc = method.getSourceCode();
+        
+        jsc.add("return ");
+        jsc.append(getName());
+        jsc.append(".remove(");
+        jsc.append(getContentName());
+        jsc.append(");");
+        
+    } //-- createRemoveByObjectMethod
+    
+    /** 
+     * Creates implementation of remove(int i) method.
+    **/
+    public void createRemoveByIndexMethod(JMethod method) {
+        
+        JSourceCode jsc = method.getSourceCode();
+        JType jType = method.getReturnType();
+        
+        jsc.add("Object obj = ");
+        jsc.append(getName());
+        jsc.append(".get(index);");
+        jsc.add(getName());
+        jsc.append(".remove(index);");
+        jsc.add("return ");
+        if (getContentType().getType() == XSType.CLASS) {
+            jsc.append("(");
+            jsc.append(jType.getName());
+            jsc.append(") obj;");
+        }
+        else {
+            jsc.append(getContentType().createFromJavaObjectCode("obj"));
+            jsc.append(";");
+        }
+        
+    } //-- createRemoveByIndexMethod
+
+    /** 
+     * Creates implementation of removeAll() method.
+    **/
+    public void createRemoveAllMethod (JMethod method) {
+
+        JSourceCode jsc = method.getSourceCode();
+        jsc.add(getName());
+        jsc.append(".clear();");
+        
+    } //-- createRemoveAllMethod
 
     /** 
      * Creates implementation of clear() method.
