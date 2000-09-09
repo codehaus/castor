7259b6db51596e5e21d4ae2f759f707fcb2127ce
yip <yip@b24b0d9a-6811-0410-802a-946fa971d308>
2000-09-09 00:33:16 +0000

    Added an empty test case for Multiple Primary Keys.

---
+++ branches/castorone/castor-2002/castor/src/tests/jdo/MultiPKs.java
@@ -0,0 +1,125 @@
+/**
+ * Redistribution and use of this software and associated documentation
+ * ("Software"), with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain copyright
+ *    statements and notices.  Redistributions must also contain a
+ *    copy of this document.
+ *
+ * 2. Redistributions in binary form must reproduce the
+ *    above copyright notice, this list of conditions and the
+ *    following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 3. The name "Exolab" must not be used to endorse or promote
+ *    products derived from this Software without prior written
+ *    permission of Exoffice Technologies.  For written permission,
+ *    please contact info@exolab.org.
+ *
+ * 4. Products derived from this Software may not be called "Exolab"
+ *    nor may "Exolab" appear in their names without prior written
+ *    permission of Exoffice Technologies. Exolab is a registered
+ *    trademark of Exoffice Technologies.
+ *
+ * 5. Due credit should be given to the Exolab Project
+ *    (http://www.exolab.org/).
+ *
+ * THIS SOFTWARE IS PROVIDED BY EXOFFICE TECHNOLOGIES AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * EXOFFICE TECHNOLOGIES OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Copyright 1999 (C) Exoffice Technologies Inc. All Rights Reserved.
+ *
+ * $Id$
+ */
+
+
+package jdo;
+
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Collection;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.lang.Math;
+import java.util.Vector;
+import java.util.Random;
+import org.exolab.castor.jdo.Database;
+import org.exolab.castor.jdo.OQLQuery;
+import org.exolab.castor.jdo.QueryResults;
+import org.exolab.castor.jdo.PersistenceException;
+import org.exolab.castor.jdo.QueryException;
+import org.exolab.castor.jdo.LockNotGrantedException;
+import org.exolab.castor.jdo.TransactionAbortedException;
+import org.exolab.castor.jdo.TransactionNotInProgressException;
+import org.exolab.castor.jdo.ObjectModifiedException;
+import org.exolab.castor.jdo.DuplicateIdentityException;
+import org.exolab.jtf.CWVerboseStream;
+import org.exolab.jtf.CWTestCase;
+import org.exolab.jtf.CWTestCategory;
+import org.exolab.exceptions.CWClassConstructorException;
+import java.util.ArrayList;
+
+
+/**
+ * Concurrent access test. Tests a JDO modification and concurrent
+ * JDBC modification to determine if JDO can detect the modification
+ * with dirty checking.
+ */
+public class MultiPKs extends CWTestCase {
+
+    private Database       _db;
+
+
+    private Connection     _conn;
+
+
+    private JDOCategory    _category;
+
+
+    public MultiPKs( CWTestCategory category )
+        throws CWClassConstructorException
+    {
+        super( "TC22", "Multiple Columns Primary Keys" );
+        _category = (JDOCategory) category;
+    }
+
+    public void preExecute()
+    {
+        super.preExecute();
+    }
+
+    public void postExecute()
+    {
+        super.postExecute();
+    }
+
+
+    public boolean run( CWVerboseStream stream ) {
+        boolean result = true;
+        try {
+			
+
+        } catch ( Exception e ) {
+            result = false;
+            e.printStackTrace();
+        }
+		// if it is not test, assume it's broken. So return false.
+        return false;
+    }
+}
+
+
