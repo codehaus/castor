a42e0970477a6656b72095ac2df2cf4aeb66b02f
oleg <oleg@b24b0d9a-6811-0410-802a-946fa971d308>
2000-09-29 10:56:20 +0000

    mapping.xml containing only "include" elements is now allowed

--- trunk/castor-2002/castor/src/main/org/exolab/castor/mapping/xml/MappingRootDescriptor.java
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/mapping/xml/MappingRootDescriptor.java
@@ -179,13 +179,12 @@ public class MappingRootDescriptor implements org.exolab.castor.xml.XMLClassDesc
             }
         } );
         desc.setHandler(handler);
-        desc.setRequired(true);
         desc.setMultivalued(true);
         elements[2] = desc;
         
         //-- validation code for: _classMappingList
         fieldValidator = new FieldValidator();
-        fieldValidator.setMinOccurs(1);
+        fieldValidator.setMinOccurs(0);
         desc.setValidator(fieldValidator);
         
         //-- _keyGeneratorDefList
