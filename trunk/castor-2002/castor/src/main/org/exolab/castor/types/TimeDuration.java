3565aa6d650ce109dec3b0eef358225e606c0ad5
blandin <blandin@b24b0d9a-6811-0410-802a-946fa971d308>
2000-10-26 00:51:12 +0000

    new parse method

--- trunk/castor-2002/castor/src/main/org/exolab/castor/types/TimeDuration.java
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/types/TimeDuration.java
@@ -40,7 +40,11 @@
  *
  * Copyright 1999-2000 (C) Intalio, Inc. All Rights Reserved.
  *
- * $Id $
+ * $Id$
+ * Date         Author           Changes
+ * 10/26/2000   Arnaud Blandin   add TimeDuration(long l)
+ * 10/26/2000   Arnaud Blandin   change parse method
+ * 10/23/2000   Arnaud Blandin   Created
  */
 
 package org.exolab.castor.types;
@@ -48,39 +52,124 @@ package org.exolab.castor.types;
 import java.text.ParseException;
 
 /**
- * Represents timeDuration utterly
- * TODO List :
- * <ul>
- *      <li> Check the validity of the fields</li>
- *      <li> write the constructor TimeDuration (long l)</li>
- *      <li> add a setValue(long l) method</li>
- *      <li> optimize the parse method </li>
- *      <li> write the full Javadoc and follow Exolab Conventions </li>
+ * Represents the timeDuration XML Schema type.
+ * This representation does not support the decimal fraction
+ * for the lowest order item.
+ * TODO suuport the corresponding facets
  * @author <a href="mailto:blandin@intalio.com">Arnaud Blandin</a>
- * @version $Revision$
+ * @version $Revision$ $Date$
 **/
 public class TimeDuration  {
 
-     /** Set to true and recompile to include debugging code in class. */
+    /** Set to true and recompile to include debugging code in class. */
     private static final boolean DEBUG = false;
-
+    /**the flag representing the 'T' position*/
+    private static final int TIME_FLAG = 8;
     //Private variables
+
+    /**
+     * the number of years
+     */
     private short _year = 0;
+    /**
+     * the number of months
+     */
     private short _month = 0;
+    /**
+     * the number of days
+     */
     private short _day = 0;
+    /**
+     * the number of hours
+     */
     private short _hour = 0;
+    /**
+     * the number of minutes
+     */
     private short _minute = 0;
+    /**
+     * the number of seconds
+     */
     private short _second = 0;
+    /**
+     * true if the Time Duration is negative
+     */
     private boolean _isNegative = false;
 
+    /**
+     * default constructor
+     */
     public TimeDuration() {
     }
 
-    /**
-     * Set the fields to the proper value
-     * TODO
+     /**
+     * <p>This constructor fills in the time duration fields according to the
+     * value of the long by calling setValue
+     * @see #setValue
+     * @param l the long value of the Time Duration
+     * @return A time duration which represents the long
      */
     public TimeDuration(long l) {
+
+        long refSecond = 1000;
+        long refMinute = 60 * refSecond;
+        long refHour = 60 * refMinute;
+        long refDay = 24 * refHour;
+        long refMonth = (long) (30.42 * refDay);
+        long refYear = 12 * refMonth;
+
+        if (DEBUG) {
+            System.out.println("In time duration Constructor");
+            System.out.println("long : "+l);
+        }
+
+        if (l<0) {
+            this.setNegative();
+            l =-l;
+        }
+
+        short year = (short) (l/refYear);
+        l = l % refYear;
+        if (DEBUG) {
+            System.out.println("nb years:"+year);
+            System.out.println("New long : "+l);
+        }
+
+        short month = (short) (l/refMonth);
+        l = l % refMonth;
+        if (DEBUG) {
+            System.out.println("nb months:"+month);
+            System.out.println("New long : "+l);
+            System.out.println(refDay);
+        }
+
+        short day = (short) (l/refDay);
+        l = l % refDay;
+         if (DEBUG) {
+            System.out.println("nb days:"+day);
+            System.out.println("New long : "+l);
+        }
+
+        short hour = (short) (l/refHour);
+        l = l % refHour;
+        if (DEBUG) {
+            System.out.println("nb hours:"+hour);
+            System.out.println("New long : "+l);
+        }
+
+        short minute = (short) (l/refMinute);
+        l = l % refMinute;
+        if (DEBUG) {
+            System.out.println("nb minutes:"+minute);
+            System.out.println("New long : "+l);
+        }
+
+        short seconds = (short) (l/refSecond);
+          if (DEBUG) {
+            System.out.println("nb seconds:"+seconds);
+        }
+
+        this.setValue(year, month, day, hour, minute, seconds);
     }
 
     //Set methods
@@ -112,6 +201,28 @@ public class TimeDuration  {
         _isNegative = true;
     }
 
+    /**
+     * Fill in the fields of the TimeDuration with the given values
+     * @param year the year value
+     * @param month the month value
+     * @param day the day value
+     * @param hour the hour value
+     * @param minute the minute value
+     * @param second the second value
+     */
+     public void setValue(short year, short month, short day,
+                         short hour, short minute, short second)
+    {
+        this.setYear(year);
+        this.setMonth(month);
+        this.setDay(day);
+        this.setHour(hour);
+        this.setMinute(minute);
+        this.setSeconds(second);
+    }
+
+
+
     //Get methods
      public short getYear() {
         return(_year);
@@ -142,15 +253,16 @@ public class TimeDuration  {
     }
 
    /**
-    * Convert a timeDuration to a long
-    * which represents the duration in milliseconds
+    * <p>Convert a timeDuration into a long
+    * This long represents the duration in milliseconds
+    * @return a long representing the duration
     */
     public long toLong() {
         long result = 0;
-        //30.416 days in a month--> to change
+        //30.42 days in a month (365/12)
         //Horner method
-        result = ( (long) ( ((((( (_year*12L) +_month )
-                                    +_day)*730L  //730 = 24 (#hours in a day) * 30.416(#days in a month)
+        result = ( (long) ( ((((( (_year*12L) +_month ) * 30.42
+                                    +_day)*24L
                                     +_hour)*60L
                                     +_minute)*60L
                                     +_second)*1000L ));
@@ -160,20 +272,35 @@ public class TimeDuration  {
     }
 
     /**
-     * Convert a timeDuration to a String
-     * conforming to ISO8601 and Schema specs
+     * <p> Convert a timeDuration into a String
+     * conforming to ISO8601 and
+     * <a href="http://www.w3.org/TR/2000/WD-xmlschema-2-20000922/#timeDuration">
+     * XML Schema specs </a>
+     *@return a string representing the time duration
      */
      public String toString() {
-        String result = null;
-        result = "P"+_year+"Y";
-        result = result + _month+"M";
-        result = result + _day+"D";
+        String result = "P";
+        if (_year != 0) {
+            result = result+_year+"Y";
+        }
+        if (_month != 0) {
+           result = result + _month+"M";
+        }
+        if (_day !=0 ) {
+            result = result + _day+"D";
+        }
         boolean isThereTime = ( (_hour != 0) || (_minute != 0) || (_second != 0) );
         if (isThereTime) {
             result = result + "T";
-            result = result + _hour +"H";
-            result = result + _minute +"M";
-            result = result + _second +"S";
+            if (_hour !=0 ) {
+                result = result + _hour +"H";
+            }
+            if (_minute !=0) {
+                result = result + _minute +"M";
+            }
+            if (_second != 0) {
+                result = result + _second +"S";
+            }
         }
         result = (_isNegative) ? "-" + result : result;
         return result;
@@ -184,107 +311,214 @@ public class TimeDuration  {
      * which represents this string
      * @param String str the string to parse
      * @return a TimeDuration instance which represent the string
-     * @throws ParseException
+     * @throws ParseException thrown when the string is not valid
      */
-    public static TimeDuration parse (String str) throws ParseException {
+     public static TimeDuration parse (String str) throws ParseException
+    {
+
+
+        if (str == null) {
+            throw new IllegalArgumentException("the string to be parsed must not"
+                                                +" be null");
+
+        }
 
         TimeDuration result = new TimeDuration();
-        int toProceed = 0;
-        int lastProceed = 0;
-        String temp = null;
-        boolean isThereTime = (str.indexOf("T") != -1);
-        //Remove the '-' if so
-        if (str.startsWith("-")) {
+        char[] chars = str.toCharArray();
+        int idx = 0;
+
+
+        if (chars.length == 0) {
+            throw new IllegalArgumentException("the string to be parsed must not"
+                                                +" be empty");
+        }
+
+        if (chars[idx] == '-') {
+            ++idx;
             result.setNegative();
-            str=str.substring(1);
-        }
-
-        if (DEBUG) {
-            System.out.println("in parse method of TimeDuration");
-            System.out.println("String to parse : "+str);
-            System.out.println("Time ? :"+isThereTime);
-            System.out.println("isNegative? : "+result.isNegative());
-        }
-
-        //Test if the string is correct
-        if (!str.startsWith("P"))
-            throw new ParseException(str+": A time duration must begin with a P",0);
-
-        if ( (!isThereTime) && ((str.indexOf("H") != -1) ||
-                                ( (str.lastIndexOf("M") != -1) &&
-                                  (str.lastIndexOf("M") > str.indexOf("T")) )||
-                                (str.indexOf("S") != -1)))
-            throw new ParseException(str+": the time duration must contain a T",0);
-        if ( (isThereTime) && ( str.substring(str.indexOf("T")+1)).equals("") )
-            throw new ParseException(str+": the 'T' must be omitted",0);
-
-        //proceed date
-        lastProceed = str.indexOf("P");
-        toProceed = str.indexOf("Y");
-        if (toProceed != -1) {
-            if (DEBUG) {
-                System.out.println("Year in process");
+            if (idx >= chars.length) {
+                throw new ParseException("'-' is wrong placed",0);
             }
-            temp = str.substring(lastProceed+1,toProceed);
-            result.setYear(Short.parseShort(temp) );
-            lastProceed = str.indexOf("Y");
-        }
-        toProceed = str.indexOf("M");
-        if ( (toProceed != -1) && (toProceed < str.indexOf("T")) )
-        {
-             if (DEBUG) {
-                System.out.println("Month in process");
-            }
-            temp = str.substring(lastProceed+1,toProceed);
-            result.setMonth(Short.parseShort(temp));
-            lastProceed = str.indexOf("M");
-        }
-        toProceed = str.indexOf("D");
-        if (toProceed != -1) {
-             if (DEBUG) {
-                System.out.println("Day in process");
-            }
-             temp = str.substring(lastProceed+1,toProceed);
-             result.setDay(Short.parseShort(temp));
         }
 
-        //proceed time
-        if (isThereTime) {
-            toProceed = str.indexOf("H");
-            if (toProceed != -1) {
-                 if (DEBUG) {
-                    System.out.println("Hour in process");
-                 }
-                temp = str.substring(str.indexOf("T")+1,toProceed);
-                result.setHour(Short.parseShort(temp));
-                lastProceed = str.indexOf("H");
+        //-- make sure we start with 'P'
+        if (chars[idx] != 'P') {
+            throw new ParseException("Missing 'P' delimiter", idx);
+        }
+        ++idx;
+
+
+        int number = 0;
+        boolean hasNumber = false;
+
+        //-- parse flags
+        // YMDTHMS = b1111111 (127)
+        // Year = 64, Month = 32, etc
+        int flags = 0;
+
+        while (idx < chars.length) {
+
+            char ch = chars[idx++];
+
+            switch(ch) {
+
+                //-- Year
+                case 'Y':
+                    //-- check for error
+                    if (flags > 0) {
+                        String err = str+":Syntax error, 'Y' must " +
+                            "proceed all other delimiters.";
+                        throw new ParseException(err, idx);
+                    }
+                    //--set flags
+                    flags = 64;
+                    if (hasNumber) {
+                        result.setYear((short)number);
+                        hasNumber = false;
+                    }
+                    else {
+                        String err = str+":missing number of years before 'Y'";
+                        throw new ParseException(err, idx);
+                    }
+
+                    break;
+                //-- Month or Minute
+                case 'M':
+                    //-- Either month or minute, check for T flag,
+                    //-- if present then Minute, otherwise Month
+                    if ((flags & TIME_FLAG) == 8) {
+
+                        // make sure no existing minute or second
+                        // flags have been set.
+                        if ((flags & 3) > 0) {
+                            throw new ParseException(str+": Syntax Error...", idx);
+                        }
+                        flags = flags | 2;
+                        if (hasNumber) {
+                            result.setMinute((short)number);
+                            hasNumber = false;
+                        }
+                        else {
+                            String err =str+": missing number of minutes " +
+                                "before 'M'";
+                            throw new ParseException(err, idx);
+                        }
+                    }
+                    //-- Month
+                    else {
+                        // make sure no existing month, day or time
+                        // flags have been set
+                        if ((flags & 63) > 0) {
+                            throw new ParseException(str+":Syntax Error...", idx);
+                        }
+                        flags = flags | 32;
+                        if (hasNumber) {
+                            result.setMonth((short)number);
+                            hasNumber = false;
+                        }
+                        else {
+                            String err = str+":missing number of months before 'M'";
+                            throw new ParseException(err, idx);
+                        }
+                    }
+                    break;
+                //-- Day
+                case 'D':
+                    // make sure no day or time flags have been set
+                    if ((flags & 31) > 0) {
+                        throw new ParseException(str+":Syntax Error...", idx);
+                    }
+                    flags = flags | 16;
+                    if (hasNumber) {
+                        result.setDay((short)number);
+                        hasNumber = false;
+                    }
+                    else {
+                        String err = str+":missing number of days before 'D'";
+                        throw new ParseException(err, idx);
+                    }
+                    break;
+                //-- Time
+                case 'T':
+                    // make sure no T flag already exists
+                    if ((flags & TIME_FLAG) == 8) {
+                        String err =str + ":Syntax error, 'T' may not " +
+                            "exist more than once.";
+                        throw new ParseException(err, idx);
+                    }
+                    flags = flags | 8;
+                    break;
+                //-- Hour
+                case 'H':
+                    // make sure no time flags have been set, but
+                    // that T exists
+                    if ((flags & 15) != 8) {
+                        String err = null;
+                        if ((flags & 8) != 8)
+                            err = str+": Missing 'T' before 'H'";
+                        else
+                            err = str+": Syntax Error, 'H' must appear for 'M' or 'S'";
+                        throw new ParseException(err, idx);
+                    }
+                    flags = flags | 4;
+                    if (hasNumber) {
+                        result.setHour((short)number);
+                        hasNumber = false;
+                    }
+                    else {
+                        String err =str+":missing number of hours before 'H'";
+                        throw new ParseException(err, idx);
+                    }
+                    break;
+                case 'S':
+                    // make sure T exists, but no 'S'
+                    if ((flags & 8) != 8) {
+                        String err = str+": Missing 'T' before 'S'";
+                        throw new ParseException(err, idx);
+                    }
+                    if ((flags & 1) == 1) {
+                        String err =str+": Syntax error 'S' may not exist more than once.";
+                        throw new ParseException(err, idx);
+                    }
+
+                    flags = flags | 1;
+                    if (hasNumber) {
+                        result.setSeconds((short)number);
+                        hasNumber = false;
+                    }
+                    else {
+                        String err = str+": missing number of seconds before 'S'";
+                        throw new ParseException(err, idx);
+                    }
+                    break;
+                default:
+                    // make sure ch is a digit...
+                    if (('0' <= ch) && (ch <= '9')) {
+
+                        if (hasNumber)
+                            number = (number*10)+(ch-48);
+                        else {
+                            hasNumber = true;
+                            number = (ch-48);
+                        }
+                    }
+                    else
+                        throw new ParseException(str+":Invalid character: " + ch, idx);
+                    break;
             }
-            toProceed = str.lastIndexOf("M");
-            if ( (toProceed != -1) &&
-                 (toProceed > str.indexOf("T")) )
-            {
-                 if (DEBUG) {
-                    System.out.println("Minute in process");
-                 }
-                temp = str.substring(lastProceed+1,str.lastIndexOf("M"));
-                result.setMinute( Short.parseShort(temp));
-                lastProceed = str.lastIndexOf("M");
-            }
-            toProceed = str.indexOf("S");
-            if (toProceed != -1) {
-                 if (DEBUG) {
-                    System.out.println("Second in process");
-                 }
-                temp = str.substring(lastProceed+1,toProceed);
-                result.setSeconds(Short.parseShort(temp));
-           }
         }
 
-       return result;
+        //-- check for T, but no HMS
+        if ((flags & 15) == 8) {
+            throw new ParseException(str+": T must be omitted", idx);
+        }
+
+        if (hasNumber) {
+            throw new ParseException(str+": expecting ending delimiter", idx);
+        }
+
+        return result;
 
     } //parse
-}
 
-
-
-
+}//TimeDuration
