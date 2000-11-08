80de781b920c23f52596b0c3241647d028569ea4
oleg <oleg@b24b0d9a-6811-0410-802a-946fa971d308>
2000-11-08 12:35:50 +0000

    getObject now hides the bug in Interbase JDBC driver (contributed by Nic Wise <nwise@inprise.co.nz>)

--- trunk/castor-2002/castor/src/main/org/exolab/castor/jdo/engine/SQLTypes.java
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/jdo/engine/SQLTypes.java
@@ -354,7 +354,8 @@ public final class SQLTypes
             boolVal = rs.getBoolean( index );
             return ( rs.wasNull() ? null : new Boolean( boolVal ) );
         default:               
-            return rs.getObject( index ); 
+            value = rs.getObject( index );
+            return ( rs.wasNull()? null : value );
         }
     }
 
