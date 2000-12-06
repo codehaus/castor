a8eb10f164edc8df04d41a47d356d2b624c6f138
kvisco <kvisco@b24b0d9a-6811-0410-802a-946fa971d308>
2000-12-06 23:48:47 +0000

    changed JMember to JField, made JMember an interface to more closely resemble the Java Reflection API

--- trunk/castor-2002/castor/src/main/org/exolab/javasource/JMember.java
+++ trunk/castor-2002/castor/src/main/org/exolab/javasource/JMember.java
@@ -38,7 +38,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Copyright 1999 (C) Intalio, Inc. All Rights Reserved.
+ * Copyright 1999-2000 (C) Intalio, Inc. All Rights Reserved.
  *
  * $Id$
  */
@@ -48,151 +48,32 @@ package org.exolab.javasource;
 import java.util.Vector;
 
 /**
+ * An interface which represents a Member of a JClass,
+ * modelled closely after the Java Reflection API.
+ * This class is part of a package used to represent
+ * source code.
  * @author <a href="mailto:kvisco@intalio.com">Keith Visco</a>
  * @version $Revision$ $Date$
 **/
-public class JMember {
-
-    /**
-     * The set of modifiers for this JMember
-    **/
-    private JModifiers modifiers = null;
-
-    private JType type = null;
-
-    private String name = null;
-
-    private JDocComment comment = null;
-
-    private String initString = null;
-    
-    /**
-     * The Class in this JMember has been declared
-    **/
-    private JClass declaringClass = null;
-
-    public JMember(JType type, String name) {
-        
-        this.type = type;
-        this.name = name;
-        this.modifiers = new JModifiers();
-        this.modifiers.makePrivate();
-        
-    } //-- JMember
-
-    
-
-    /**
-     * Returns the comment describing this member. 
-     * @return the comment describing this member, or
-     * null if no comment has been set.
-    **/
-    public JDocComment getComment() {
-        return this.comment;
-    } //-- getComment
-
-    
+interface JMember {
 
     /**
      * Returns the class in which this JMember has been declared
      * @return the class in which this JMember has been declared
     **/
-    public JClass getDeclaringClass() {
-        return this.declaringClass;
-    } //-- getDeclaringClass
+    public JClass getDeclaringClass();
 
-    /**
-     * Returns the initialization String for this JMember
-     * @return the initialization String for this JMember,
-     * or null if no initialization String was specified.
-    **/
-    public String getInitString() {
-        return initString;
-    } //-- getInitString
-    
-     
     /**
      * Returns the modifiers for this JMember
      * @return the modifiers for this JMember     
     **/
-    public JModifiers getModifiers() {
-        return this.modifiers;
-    } //-- getModifiers
+    public JModifiers getModifiers();
 
     /**
      * Returns the name of this JMember
      * @return the name of this JMember
     **/
-    public String getName() {
-        return this.name;
-    } //-- getName
-
-    /**
-     * Returns the JType represting the type of this JMember
-     * @return the JClass represting the type of this JMember
-    **/
-    public JType getType() {
-        return this.type;
-    } //-- getType
-
-    /**
-     * Sets the comment describing this member. 
-     * @param comment the JDocComment for this member
-    **/
-    public void setComment(JDocComment comment) {
-        this.comment = comment;
-    } //-- setComment
-
-    /**
-     * Sets the comment describing this member. 
-     * @param comment the JDocComment for this member
-    **/
-    public void setComment(String comment) {
-        if (this.comment == null) {
-            this.comment = new JDocComment();
-        }
-        this.comment.setComment(comment);
-    } //-- setComment
-
-    /**
-     * Sets the initialization string for this JMember;
-     * Allows some flexibility in declaring default values.
-     * @param init the initialization string for this member.
-    **/
-    public void setInitString(String init) {
-        this.initString = init;
-    } //-- setInitString
-    
-    /**
-     * Sets the name of this JMember
-     * @param name the name of this JMember
-     * @exception IllegalArgumentException when the
-     * name is not a valid Java member name, or if a member
-     * with the given name already exists in the declaring class
-    **/
-    public void setName(String name) throws 
-        IllegalArgumentException
-    {
-        this.name = name;
-    } //-- setName
-
-    public void setModifiers(JModifiers modifiers) {
-        this.modifiers = modifiers;
-    } //-- setModifiers
-
-    protected void setDeclaringClass(JClass declaringClass) {
-        this.declaringClass = declaringClass;
-    } //-- setDeclaringClass
-   
-    public String toString() {
-        StringBuffer sb = new StringBuffer();
-        sb.append(modifiers.toString());
-        sb.append(' ');
-        sb.append(type);
-        sb.append(' ');
-        sb.append(name);
-        return sb.toString();
-    } //-- toString
+    public String getName();
 
 } //-- JMember
 
