c667dabed733564ce29946a1b81f4bc4ed21dad2
kvisco <kvisco@b24b0d9a-6811-0410-802a-946fa971d308>
2000-12-07 23:39:29 +0000

    increased the command line parameters to 9, fixed the classpath issue

--- trunk/castor-2002/castor/src/etc/sourceGen.bat
+++ trunk/castor-2002/castor/src/etc/sourceGen.bat
@@ -3,9 +3,10 @@ REM $Id$
 set JAVA=%JAVA_HOME%\bin\java
 set cp=%CLASSPATH%
 for %%i in (lib\*.jar) do call cp.bat %%i
-set cp=%cp%:.
+set cp=%cp%;.
 
-%JAVA% -classpath %CP% org.exolab.castor.builder.SourceGenerator %1 %2 %3 %4 %5 %6
+
+%JAVA% -classpath %CP% org.exolab.castor.builder.SourceGenerator %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 
 
