18bb0f15d01fa0c45297c70ac3b58e7f99fc7018
gignoux <gignoux@b24b0d9a-6811-0410-802a-946fa971d308>
2001-01-10 02:35:41 +0000

    Revert the changes made in version 1.53 : this is probably not the good way to implements it

--- trunk/castor-2002/castor/src/main/org/exolab/castor/mapping/loader/MappingLoader.java
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/mapping/loader/MappingLoader.java
@@ -803,7 +803,7 @@ public abstract class MappingLoader
 
                 if ( fieldType == null )
                     fieldType = Types.typeFromPrimitive( method.getReturnType() );
-                else if ( ! Types.typeFromPrimitive( method.getReturnType() ).isAssignableFrom( Types.typeFromPrimitive( fieldType ) ) )
+                else if ( ! Types.typeFromPrimitive( fieldType ).isAssignableFrom( Types.typeFromPrimitive( method.getReturnType() ) ) )
                     throw new MappingException( "mapping.accessorReturnTypeMismatch",
                                                 method, fieldType.getName() );
             } else {
