70d7392ae1070bc6ed00dc8edac175b27faab863
yip <yip@b24b0d9a-6811-0410-802a-946fa971d308>
2001-02-27 21:48:01 +0000

    Fixed the "goto" labels.

--- trunk/castor-2002/castor/oqlquery.bat
+++ trunk/castor-2002/castor/oqlquery.bat
@@ -9,8 +9,10 @@ for %%i in (lib\*.jar) do call cp.bat %%i
 if not "%2"=="" goto run
 echo Usage: oqlquery.bat DatabaseName DatabaseConfig
 echo Example: oqlquery.bat test file:build/examples/jdo/database.xml
-exit 1
+goto end
 
-run:
-%JAVA% -classpath %CP% org.exolab.castor.tools.QueryAnalyser %1 %2
+:run
+%JAVA% -classpath %CP% org.exolab.castor.gui.QueryAnalyser %1 %2
+
+:end
 set CLASSPATH=%OLDCP%
