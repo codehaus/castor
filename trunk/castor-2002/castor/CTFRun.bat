e0b8ed80ce9d83894bec99b29fc3ff13d40cca55
psq <psq@b24b0d9a-6811-0410-802a-946fa971d308>
2001-03-29 00:53:47 +0000

    fixed for windows

--- trunk/castor-2002/castor/CTFRun.bat
+++ trunk/castor-2002/castor/CTFRun.bat
@@ -5,6 +5,8 @@ set OLDCP=%CLASSPATH%
 set CLASSPATH=build\classes;build\tests;%CLASSPATH%;%JAVA_HOME%\lib\tools.jar
 set cp=%CLASSPATH%
 for %%i in (lib\*.jar) do call cp.bat %%i
-deltree /Y .\build\tests\output
+rmdir /q /s .\build\tests\output>nul
+echo %CP%
 %JAVA% -classpath %CP% org.exolab.castor.tests.framework.CastorTestSuiteRunner %1 %2 %3  %4 %5 %6 %7
 set CLASSPATH=%OLDCP%
+
