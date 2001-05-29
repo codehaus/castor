da801ee1e30fcbaa64ca2533b34048891257378b
blandin <blandin@b24b0d9a-6811-0410-802a-946fa971d308>
2001-05-29 21:31:04 +0000

    throw Exception when a built-in type has not been declared in the Schema Namespace

--- trunk/castor-2002/castor/src/main/org/exolab/castor/xml/schema/Schema.java
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/xml/schema/Schema.java
@@ -669,6 +669,13 @@ public class Schema extends Annotated {
         if (ns == null) {
             //-- first try built-in types
             result= simpleTypesFactory.getBuiltInType(name);
+            //if we have a built-in type not declared in the good namespace -> Exception
+             if ( (result != null) && (namespaces.containsValue(DEFAULT_SCHEMA_NS))) {
+                String err = "getSimpleType: the simple type '"+name+
+                             "' has not been declared in XML Schema namespace.";
+                throw new IllegalArgumentException(err);
+            }
+
             //-- otherwise check user-defined types
             if (result == null)
                 result = (SimpleType)simpleTypes.get(name);
