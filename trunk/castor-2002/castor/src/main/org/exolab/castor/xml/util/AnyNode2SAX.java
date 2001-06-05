38db0bf7de2fe466647afe01c19476b576a9970a
blandin <blandin@b24b0d9a-6811-0410-802a-946fa971d308>
2001-06-05 23:26:56 +0000

    handle mixed content + added licence

--- trunk/castor-2002/castor/src/main/org/exolab/castor/xml/util/AnyNode2SAX.java
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/xml/util/AnyNode2SAX.java
@@ -1,3 +1,50 @@
+/**
+ * Redistribution and use of this software and associated documentation
+ * ("Software"), with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain copyright
+ *    statements and notices.  Redistributions must also contain a
+ *    copy of this document.
+ *
+ * 2. Redistributions in binary form must reproduce the
+ *    above copyright notice, this list of conditions and the
+ *    following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 3. The name "Exolab" must not be used to endorse or promote
+ *    products derived from this Software without prior written
+ *    permission of Intalio, Inc.  For written permission,
+ *    please contact info@exolab.org.
+ *
+ * 4. Products derived from this Software may not be called "Exolab"
+ *    nor may "Exolab" appear in their names without prior written
+ *    permission of Intalio, Inc. Exolab is a registered
+ *    trademark of Intalio, Inc.
+ *
+ * 5. Due credit should be given to the Exolab Project
+ *    (http://www.exolab.org/).
+ *
+ * THIS SOFTWARE IS PROVIDED BY INTALIO, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * INTALIO, INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Copyright 2001 (C) Intalio, Inc. All Rights Reserved.
+ *
+ * $Id$
+ * Date         Author              Changes
+ * 04/06/2001   Arnaud Blandin      Created
+ */
+
 package org.exolab.castor.xml.util;
 
 import org.xml.sax.DocumentHandler;
@@ -8,15 +55,12 @@ import org.exolab.castor.util.Stack;
 
 import org.xml.sax.helpers.AttributeListImpl;
 import org.xml.sax.SAXException;
-/**
- * Title:
- * Description:
- * Copyright:    Copyright (c) 2001
- * Company:
- * @author
- * @version 1.0
- */
 
+/**
+ * A class for converting an AnyNode to SAX events
+ * @author <a href="mailto:blandin@intalio.com">Arnaud Blandin</a>
+ * @version $Revision$ $Date$
+ */
 public class AnyNode2SAX implements EventProducer {
 
    /**
@@ -159,9 +203,16 @@ public class AnyNode2SAX implements EventProducer {
 
 
        }//ELEMENTS
-        else if (node.getNodeType() == AnyNode.TEXT)
-            processTextNode(node, handler);
-
+        else {
+             if (node.getNodeType() == AnyNode.TEXT)
+                  processTextNode(node, handler);
+             AnyNode tempNode = node.getNextSibling();
+                while (tempNode != null) {
+                     processAnyNode(tempNode, handler);
+                     tempNode = tempNode.getNextSibling();
+                }
+              tempNode = null;
+        }
     }
 
     private void processTextNode(AnyNode node, DocumentHandler handler)
