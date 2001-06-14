4a300ee7f93b85c49eeb932cb6aa00901df9aa94
blandin <blandin@b24b0d9a-6811-0410-802a-946fa971d308>
2001-06-14 00:05:13 +0000

    no message

--- trunk/castor-2002/castor/CTFRun.bat
+++ trunk/castor-2002/castor/CTFRun.bat
@@ -2,7 +2,7 @@
 REM $Id$
 set JAVA=%JAVA_HOME%\bin\java
 set OLDCP=%CLASSPATH%
-set CLASSPATH=build\classes;build\tests;%CLASSPATH%;%JAVA_HOME%\lib\tools.jar
+set CLASSPATH=.\build\classes;.\build\tests;%CLASSPATH%;%JAVA_HOME%\lib\tools.jar
 set cp=%CLASSPATH%
 for %%i in (lib\*.jar) do call cp.bat %%i
 rmdir /q /s .\build\tests\output>nul
