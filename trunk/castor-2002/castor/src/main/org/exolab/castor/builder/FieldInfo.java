7d12b5e251ccb0b2110cedbf36dbf2b1a54a01bb
blandin <blandin@b24b0d9a-6811-0410-802a-946fa971d308>
2001-06-15 01:35:35 +0000

    add fixed value as initString

--- trunk/castor-2002/castor/src/main/org/exolab/castor/builder/FieldInfo.java
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/builder/FieldInfo.java
@@ -59,55 +59,55 @@ import java.util.Vector;
  * @version $Revision$ $Date$
 **/
 public class FieldInfo extends XMLInfo {
- 
+
     /**
      * The Java name for Members described by this FieldInfo
     **/
     private String name = null;
-    
-    
+
+
     private CodeHelper codeHelper = null;
-    
+
     private ClassInfo declaringClassInfo = null;
-    
+
     private String _comment    = null;
-    
+
     /**
      * The default value for this FieldInfo
     **/
     private String _default    = null;
-    
+
     /**
      * The fixed production for this FieldInfo
     **/
     private String _fixed      = null;
-    
+
     /**
      * A flag to indicate a final member
     **/
     private boolean _final     = false;
-    
+
     /**
      * A flag to indicate a static member
     **/
     private boolean _static    = false;
-    
+
     /**
      * Flags whether or not the a MarshalDescriptor should
      * be created for this FieldInfo
     **/
     private boolean _transient = false;
-    
+
     /**
      * A flag to indicate a bound property
     **/
     private boolean _bound = false;
-    
+
     /**
      * A flag to indicate a container field
     **/
     private boolean _isContainer = false;
-    
+
     /**
      * Creates a new FieldInfo with the given XML Schema type
      * and the given member name
@@ -123,7 +123,7 @@ public class FieldInfo extends XMLInfo {
     //------------------/
     //- Public Methods -/
     //------------------/
-    
+
     /**
      * Creates the JMembers for this FieldInfo, sometimes a "field"
      * requires more than one java field
@@ -131,39 +131,40 @@ public class FieldInfo extends XMLInfo {
      * for this FieldInfo
     **/
     public void createJavaField(JClass jClass) {
-        
+
         XSType type = getSchemaType();
-        
+
         JType jType = type.getJType();
-        
+
         JField field = new JField(type.getJType(), name);
-        
+
         if (_static || _final) {
             JModifiers modifiers = field.getModifiers();
             modifiers.setFinal(_final);
             modifiers.setStatic(_static);
         }
-        
+
         //-- set init String
-        if (_default != null) {
+        if (_default != null)
             field.setInitString(_default);
-        }
-        
-        
+
+        if (_fixed != null)
+               field.setInitString(_fixed);
+
         //-- set Javadoc comment
         if (_comment != null) field.setComment(_comment);
-        
+
         jClass.addField(field);
-        
+
         //-- special supporting fields
-        
+
         //-- has_field
         if ((!type.isEnumerated()) && type.isPrimitive()) {
             field = new JField(JType.Boolean, "_has" + name);
             field.setComment("keeps track of state for field: " + name);
             jClass.addField(field);
         }
-        
+
         //-- save default value for primitives
         //-- not yet finished
         /*
@@ -172,36 +173,36 @@ public class FieldInfo extends XMLInfo {
             JModifiers modifiers = field.getModifiers();
             modifiers.setFinal(true);
             modifiers.setStatic(true);
-            
+
             if (_default != null)
                 field.setInitString(_default);
-            
+
             jClass.addField(field);
         }
         */
-        
-        
+
+
     } //-- createJavaField
-    
-    
+
+
     /**
      * Creates the access methods for this FieldInfo
      * @param jClass the JClass to add the methods to
     **/
     public void createAccessMethods(JClass jClass) {
-        
+
         JMethod method    = null;
         JSourceCode jsc   = null;
-        
+
         String mname = methodSuffix();
-        
+
         XSType xsType = getSchemaType();
         JType jType  = xsType.getJType();
-        
-        
-        boolean needs_has 
+
+
+        boolean needs_has
             = ((!xsType.isEnumerated()) && xsType.isPrimitive());
-            
+
         //-- create get method
         method = new JMethod(jType, "get"+mname);
         jClass.addMethod(method);
@@ -209,21 +210,21 @@ public class FieldInfo extends XMLInfo {
         jsc.add("return this.");
         jsc.append(this.name);
         jsc.append(";");
-        
+
         //-- create set method
         method = new JMethod(null, "set"+mname);
         jClass.addMethod(method);
-        
+
         String paramName = this.name;
 		//-- Keith, not sure why this was needed? I've commented it out
-		//-- since it causes compile failures when a field name is used 
+		//-- since it causes compile failures when a field name is used
 		//-- that is a Java reserved word
         // if (paramName.indexOf('_') == 0)
         //   paramName = paramName.substring(1);
-        
+
         method.addParameter(new JParameter(jType, paramName));
         jsc = method.getSourceCode();
-        
+
         //-- bound properties
         if (_bound) {
             // save old value
@@ -233,15 +234,15 @@ public class FieldInfo extends XMLInfo {
 			//-- 'this.' ensures this refers to the class member not the parameter
             jsc.append(xsType.createToJavaObjectCode("this."+getName()));
             jsc.append(";");
-        } 
-        
+        }
+
         //-- set new value
         jsc.add("this.");
         jsc.append(getName());
         jsc.append(" = ");
         jsc.append(paramName);
         jsc.append(";");
-        
+
         //-- bound properties
         if (_bound) {
             //notify listeners
@@ -253,14 +254,14 @@ public class FieldInfo extends XMLInfo {
 			//-- 'this.' ensures this refers to the class member not the parameter
             jsc.append(xsType.createToJavaObjectCode("this."+getName()));
             jsc.append(");");
-        } 
-        
+        }
+
         //-- hasProperty
         if (needs_has) {
             jsc.add("this._has");
             jsc.append(getName());
             jsc.append(" = true;");
-           
+
             //-- create hasMethod
             method = new JMethod(JType.Boolean, "has"+mname);
             jClass.addMethod(method);
@@ -268,7 +269,7 @@ public class FieldInfo extends XMLInfo {
             jsc.add("return this._has");
             jsc.append(getName());
             jsc.append(";");
-			
+
 			//-- if optional then create delete method
 			if (!isRequired()) {
                 method = new JMethod(null, "delete"+mname);
@@ -276,20 +277,20 @@ public class FieldInfo extends XMLInfo {
                 jsc = method.getSourceCode();
                 jsc.add("this._has");
                 jsc.append(getName());
-                jsc.append("= false;");			
+                jsc.append("= false;");
             }
         }
-		
-        
-        
+
+
+
     } //-- createAccessMethods
-    
-    
+
+
     public CodeHelper getCodeHelper() {
         return codeHelper;
     } //-- getCodeHelper
-    
-    
+
+
     /**
      * Returns the default value for this FieldInfo
      * @return the default value for this FieldInfo, or null if no
@@ -298,7 +299,7 @@ public class FieldInfo extends XMLInfo {
     public String getDefaultValue() {
         return _default;
     } //-- getDefaultValue
-    
+
     /**
      * Returns the fixed production for this FieldInfo, or null
      * if no fixed value has been specified.
@@ -309,7 +310,7 @@ public class FieldInfo extends XMLInfo {
     public String getFixedValue() {
         return _fixed;
     } //-- getFixedValue
-    
+
     /**
      * Returns the name of the delete method for this FieldInfo.
      * @return the name of the delete method for this FieldInfo
@@ -317,7 +318,7 @@ public class FieldInfo extends XMLInfo {
     public String getDeleteMethodName() {
         return "delete" + methodSuffix();
     } //-- getDeleteMethodName
-    
+
     /**
      * Returns the name of the has method for this FieldInfo
      * @return the name of the has method for this FieldInfo
@@ -325,7 +326,7 @@ public class FieldInfo extends XMLInfo {
     public String getHasMethodName() {
         return "has" + methodSuffix();
     } //-- getHasMethodName
-    
+
     /**
      * Returns the name of the read method for this FieldInfo
      * @return the name of the read method for this FieldInfo
@@ -333,19 +334,19 @@ public class FieldInfo extends XMLInfo {
     public String getReadMethodName() {
         return "get" + methodSuffix();
     } //-- getReadMethodName
-    
-    
+
+
     /**
      * Returns the name of the write method for this FieldInfo
      * @return the name of the write method for this FieldInfo
     **/
     public String getWriteMethodName() {
-        if (isMultivalued()) 
+        if (isMultivalued())
             return "add" + methodSuffix();
         else
             return "set" + methodSuffix();
     } //-- getWriteMethodName
-    
+
     /**
      * Creates code for initialization of this Member
      * @param jsc the JSourceCode in which to add the source to
@@ -353,8 +354,8 @@ public class FieldInfo extends XMLInfo {
     public void generateInitializerCode(JSourceCode jsc) {
         //-- do nothing by default
     } //-- generateInitializerCode
-    
-    
+
+
     /**
      * Returns the comment associated with this Member
      * @return the comment associated with this Member, or null
@@ -363,7 +364,7 @@ public class FieldInfo extends XMLInfo {
     public String getComment() {
         return _comment;
     } //-- getComment
-    
+
     /**
      * Returns the name of this FieldInfo
      * @return the name of this FieldInfo
@@ -371,7 +372,7 @@ public class FieldInfo extends XMLInfo {
     public String getName() {
         return this.name;
     } //-- getName
-    
+
     /**
      * Returns true if this FieldInfo represents a bound property
      *
@@ -380,10 +381,10 @@ public class FieldInfo extends XMLInfo {
     public boolean isBound() {
         return _bound;
     } //-- isBound
-    
+
     /**
      * Returns true if this FieldInfo describes a container
-     * class. A container class is a class which should not be 
+     * class. A container class is a class which should not be
      * marshalled as XML, but whose members should be.
      *
      * @return true if this ClassInfo describes a container class.
@@ -391,17 +392,17 @@ public class FieldInfo extends XMLInfo {
     public boolean isContainer() {
         return _isContainer;
     } //-- isContainer
-    
+
     /**
      * Returns true if this FieldInfo is a transient member. Transient
-     * members are members which should be ignored by the 
+     * members are members which should be ignored by the
      * Marshalling framework
      * @return true if this FieldInfo is transient
     **/
     public boolean isTransient() {
         return (_transient || _final || _static);
     } //-- isTransient
-    
+
     /**
      * Sets the CodeHelper to use when creating source code
      * @param codeHelper the CodeHelper to use when creating source code
@@ -410,15 +411,15 @@ public class FieldInfo extends XMLInfo {
     public void setCodeHelper(CodeHelper codeHelper) {
         this.codeHelper = codeHelper;
     } //-- setCodeHelper
-    
+
     /**
      * Sets the comment for this Member
-     * @param comment the comment or description for this Member 
+     * @param comment the comment or description for this Member
     **/
     public void setComment(String comment) {
         _comment = comment;
     } //-- setComment
-    
+
     /**
      * Returns the ClassInfo to which this Member was declared,
      * for inheritance reasons
@@ -426,7 +427,7 @@ public class FieldInfo extends XMLInfo {
     public ClassInfo getDeclaringClassInfo() {
         return this.declaringClassInfo;
     } //-- getDeclaringClassInfo
-    
+
     /**
      * Sets whether or not this FieldInfo represents a bound property
      *
@@ -436,11 +437,11 @@ public class FieldInfo extends XMLInfo {
     public void setBound(boolean bound) {
         _bound = bound;
     } //-- setBound
-    
+
     /**
      * Sets whether or not this FieldInfo describes a container
-     * field. A container field is a field which should not be 
-     * marshalled directly as XML, but whose members should be. 
+     * field. A container field is a field which should not be
+     * marshalled directly as XML, but whose members should be.
      * By default this is false.
      *
      * @param isContainer the boolean value when true indicates
@@ -449,19 +450,19 @@ public class FieldInfo extends XMLInfo {
     public void setContainer(boolean isContainer) {
         _isContainer = isContainer;
     } //-- setContainer
-    
+
     public void setDeclaringClassInfo(ClassInfo declaringClassInfo) {
         this.declaringClassInfo = declaringClassInfo;
     } //-- setDeclaringClassInfo
-    
+
     /**
      * Sets the default value for this FieldInfo
-     * @param defaultValue the default value 
+     * @param defaultValue the default value
     **/
     public void setDefaultValue(String defaultValue) {
         this._default = defaultValue;
     } //-- setDefaultValue;
-     
+
     /**
      * Sets the "final" status of this FieldInfo. Final
      * members are also transient.
@@ -471,7 +472,7 @@ public class FieldInfo extends XMLInfo {
     public void setFinal(boolean isFinal) {
         this._final = isFinal;
     } //-- isFinal
-    
+
     /**
      * Sets the fixed value in which instances of this field type must
      * lexically match
@@ -482,8 +483,8 @@ public class FieldInfo extends XMLInfo {
     public void setFixedValue(String fixedValue) {
         this._fixed = fixedValue;
     } //-- setFixedValue
-    
-    /** 
+
+    /**
      * Sets the "static" status of this FieldInfo. Static
      * members are also transient.
      * @param isStatic the boolean indicating the static status,
@@ -492,16 +493,16 @@ public class FieldInfo extends XMLInfo {
     public void setStatic(boolean isStatic) {
         this._static = isStatic;
     } //-- setStatic
-    
-    /** 
-     * Sets the transient status of this FieldInfo. 
+
+    /**
+     * Sets the transient status of this FieldInfo.
      * @param isTransient the boolean indicating the transient status,
      * if true this FieldInfo will be treated as transient
     **/
     public void setTransient(boolean isTransient) {
         this._transient = isTransient;
     } //-- setTransient
-    
+
     /**
      * Returns the method suffix for creating method names.
     **/
@@ -511,6 +512,6 @@ public class FieldInfo extends XMLInfo {
         else
             return JavaNaming.toJavaClassName(name);
     }
-    
+
 } //-- FieldInfo
 
