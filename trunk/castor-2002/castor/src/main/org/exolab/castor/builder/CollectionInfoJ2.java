dd972efd58208731081649094a9d017ca76cd48a
blandin <blandin@b24b0d9a-6811-0410-802a-946fa971d308>
2001-07-19 08:58:36 +0000

    handle castor 'date' type correctly...this should not have side effects, the only result is the code is less beautiful

--- trunk/castor-2002/castor/src/main/org/exolab/castor/builder/CollectionInfoJ2.java
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/builder/CollectionInfoJ2.java
@@ -256,7 +256,7 @@ public class CollectionInfoJ2 extends CollectionInfo {
 
         jsc.add(compType.toString());
         jsc.append("[] mArray = new ");
-        jsc.append(compType.getLocalName());
+        jsc.append(compType.toString());
         jsc.append("[size]");
         //-- if component is an array, we must add [] after setting
         //-- size
