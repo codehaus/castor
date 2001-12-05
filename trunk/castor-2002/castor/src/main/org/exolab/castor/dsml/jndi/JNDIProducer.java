c96417f9715c5f1dd59d911767c572a970fa28e0
yip <yip@b24b0d9a-6811-0410-802a-946fa971d308>
2001-12-05 23:50:10 +0000

    Fixed a NPE. (bug reported and fixed by Gilbert Pilz [gilbert.pilz@e2open.com])

--- trunk/castor-2002/castor/src/main/org/exolab/castor/dsml/jndi/JNDIProducer.java
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/dsml/jndi/JNDIProducer.java
@@ -164,8 +164,6 @@ public class JNDIProducer
                                     encode = true;
                         }
                         if (encode) {
-                            MimeBase64Encoder encoder;
-                            encoder = new MimeBase64Encoder();
                             if (wchar) {
                                 bytes = new byte[chars.length << 1];
                                 int j = 0;
@@ -180,11 +178,16 @@ public class JNDIProducer
                                         bytes[i] = (byte)chars[i];
                                 }
                             }
-                            encoder.translate( bytes );
-                            chars = encoder.getCharArray();
-                            attrList.addAttribute( XML.Entries.Attributes.Encoding, "NMTOKEN",
-                                                   XML.Entries.Attributes.Encodings.Base64 );
                         }
+                    }
+
+                    if (bytes != null)
+                    {
+                        MimeBase64Encoder encoder = new MimeBase64Encoder();
+                        encoder.translate( bytes );
+                        chars = encoder.getCharArray();
+                        attrList.addAttribute( XML.Entries.Attributes.Encoding, "NMTOKEN", 
+                                               XML.Entries.Attributes.Encodings.Base64 );
                     }
                     _docHandler.startElement( prefix( XML.Entries.Elements.Value ), attrList );
                     _docHandler.characters( chars, 0, chars.length );
