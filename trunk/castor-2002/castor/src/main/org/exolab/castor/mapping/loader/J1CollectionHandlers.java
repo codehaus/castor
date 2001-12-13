e08687914a3d8e716f9bc0f1c0443622477e5946
kvisco <kvisco@b24b0d9a-6811-0410-802a-946fa971d308>
2001-12-13 09:17:14 +0000

    added basic Enumeration collection handler, only the enumerate method really pertains to this collection

--- trunk/castor-2002/castor/src/main/org/exolab/castor/mapping/loader/J1CollectionHandlers.java
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/mapping/loader/J1CollectionHandlers.java
@@ -189,7 +189,31 @@ public final class J1CollectionHandlers
             public String toString() {
                 return "Hashtable";
             }
+        } ),
+        // For Enumeration (1.1)
+        new CollectionHandlers.Info( "enumerate", Enumeration.class, false, new CollectionHandler() {
+            public Object add( Object collection, Object object ) {
+                //-- do nothing, cannot add elements to an enumeration
+                return null;
+            }
+            public Enumeration elements( Object collection ) {
+                if ( collection == null )
+                    return new CollectionHandlers.EmptyEnumerator();
+                return ( (Enumeration) collection );
+            }
+            public int size( Object collection ) {
+                //-- Nothing we can do without destroying the enumeration
+                return 0;
+            }
+            public Object clear( Object collection ) {
+                //-- Should we iterate over nextElement?
+                return null;
+            }
+            public String toString() {
+                return "Enumeration";
+            }
         } )
+        
     };
 
 
