7312e6e17c45186535d329b271ad9fdb0cc327bf
blandin <blandin@b24b0d9a-6811-0410-802a-946fa971d308>
2002-03-06 10:21:50 +0000

    added the canAccept method

--- trunk/castor-2002/castor/src/tests/harness/HarnessDescriptor.java
+++ trunk/castor-2002/castor/src/tests/harness/HarnessDescriptor.java
@@ -278,4 +278,21 @@ public class HarnessDescriptor implements org.exolab.castor.xml.XMLClassDescript
         return null;
     }
 
+ /**
+     * <p>Returns true if the given object represented by this XMLClassDescriptor
+     * can accept a member whose name is given.
+     * An XMLClassDescriptor can accept a field if it contains a descriptor that matches
+     * the given name and if the given object can hold this field (i.e a value is not already set for
+     * this field).
+     * <p>This is mainly used for container object (that can contains other object), in this particular case
+     * the implementation will return null.
+     * @param fieldName the name of the field to check
+     * @param object the object represented by this XMLCLassDescriptor
+     * @return true if the given object represented by this XMLClassDescriptor
+     * can accept a member whose name is given.
+     */
+    public boolean canAccept(String fieldName, Object object) {
+         return false;
+    }
+
 }
