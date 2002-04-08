0c6ffe67e4199d1708b174e994dd4e33573aec4c
blandin <blandin@b24b0d9a-6811-0410-802a-946fa971d308>
2002-04-08 22:26:38 +0000

    re-organize the numbering

--- trunk/castor-2002/castor/src/main/org/exolab/castor/builder/XMLInfo.java
+++ trunk/castor-2002/castor/src/main/org/exolab/castor/builder/XMLInfo.java
@@ -53,67 +53,67 @@ import org.exolab.castor.builder.types.*;
  * @version $Revision$ $Date$
 **/
 public class XMLInfo {
-    
+
     /**
      * Represents the attribute node type
     **/
     public static final short ATTRIBUTE_TYPE = 0;
-    
+
     /**
      * Represents the element node type
     **/
-    public static final short ELEMENT_TYPE   = 2;
-    
+    public static final short ELEMENT_TYPE   = 1;
+
     /**
      * Represents the text node type
     **/
-    public static final short TEXT_TYPE      = 3;
-    
-    
+    public static final short TEXT_TYPE      = 2;
+
+
     /**
      * A flag indicating if the object described by this
      * XML info can appear more than once
     **/
     private boolean multivalued = false;
-    
-    /** 
-     * the xml node name 
+
+    /**
+     * the xml node name
     **/
     private String name = null;
-    
+
     /**
      * The xml node type
     **/
     private short nodeType = ELEMENT_TYPE;
-    
+
     /**
      * The namespace prefix
     **/
     private String nsPrefix = null;
-    
+
     /**
      * The namespace URI
     **/
     private String nsURI    = null;
-    
+
     /**
      * indicates the XML object must appear at least once
     **/
     private boolean required = false;
-    
+
     /**
      * The XML Schema type
     **/
     private XSType xsType = null;
-    
-    
+
+
     /**
      * Creates a new XML Info
     **/
     public XMLInfo() {
         super();
     } //-- XMLInfo
-    
+
     /**
      * Creates a new XMLInfo with the given node type
      * @param nodeType the nodeType which this XMLInfo represents
@@ -121,7 +121,7 @@ public class XMLInfo {
     public XMLInfo(short nodeType) {
         this.nodeType = nodeType;
     }  //-- XMLInfo
-    
+
     /**
      * Creates a new XMLInfo with the given xml name and node type
      * @param name the xml node name
@@ -131,7 +131,7 @@ public class XMLInfo {
         this.nodeType = nodeType;
         this.name     = name;
     }  //-- XMLInfo
-    
+
     /**
      * Returns the XML name for the object described by this XMLInfo
      * @return the XML name for the object described by this XMLInfo,
@@ -140,8 +140,8 @@ public class XMLInfo {
     public String getNodeName() {
         return name;
     } //-- getNodeName
-    
-   
+
+
     /**
      * Returns the namespace prefix of the object described by this XMLInfo
      * @return the namespace prefix of the object described by this XMLInfo
@@ -149,7 +149,7 @@ public class XMLInfo {
     public String getNamespacePrefix() {
         return nsPrefix;
     } //-- getNamespacePrefix
-    
+
     /**
      * Returns the namespace URI of the object described by this XMLInfo
      * @return the namespace URI of the object described by this XMLInfo
@@ -157,7 +157,7 @@ public class XMLInfo {
     public String getNamespaceURI() {
         return nsURI;
     } //-- getNamespaceURI
-    
+
     /**
      * Returns the node type for the object described by this XMLInfo
      * @return the node type for the object described by this XMLInfo
@@ -165,16 +165,16 @@ public class XMLInfo {
     public short getNodeType() {
         return nodeType;
     } //-- getNodeType
-    
-    
+
+
     /**
-     * Returns the XML Schema type for the described object 
+     * Returns the XML Schema type for the described object
      * @return the XML Schema type
     **/
     public XSType getSchemaType() {
         return xsType;
     } //-- getXSType
-    
+
     /**
      * Return whether or not the object described by this XMLInfo
      * is multi-valued (appears more than once in the XML document)
@@ -183,7 +183,7 @@ public class XMLInfo {
     public boolean isMultivalued() {
         return multivalued;
     } //-- isMultivalued
-    
+
     /**
      * Return true if the XML object described by this XMLInfo must
      * appear at least once in the XML document (or object model)
@@ -192,18 +192,18 @@ public class XMLInfo {
     public boolean isRequired() {
         return required;
     } //-- isRequired
-    
-    
+
+
     /**
      * Sets whether the XML object can appear more than once in the
      * XML document
-     * @param multivalued the boolean indicating whether or not 
+     * @param multivalued the boolean indicating whether or not
      * the object can appear more than once
     **/
     public void setMultivalued(boolean multivalued) {
         this.multivalued = multivalued;
     } //-- setMultivalued
-    
+
     /**
      * Sets the XML name of the object described by this XMLInfo.
      * @param name the XML node name of the described object.
@@ -211,7 +211,7 @@ public class XMLInfo {
     public void setNodeName(String name) {
         this.name = name;
     } //-- setName
-    
+
     /**
      * Sets the desired namespace prefix for this XMLInfo
      * There is no guarantee that this prefix will be used.
@@ -220,7 +220,7 @@ public class XMLInfo {
     public void setNamespacePrefix(String nsPrefix) {
         this.nsPrefix = nsPrefix;
     } //-- setNamespacePrefix
-    
+
     /**
      * Sets the Namespace URI for this XMLInfo
      * @param nsURI the Namespace URIfor this XMLInfo
@@ -228,7 +228,7 @@ public class XMLInfo {
     public void setNamespaceURI(String nsURI) {
         this.nsURI = nsURI;
     } //-- setNamespaceURI
-   
+
     /**
      * Sets the nodeType for this XMLInfo
      * @param nodeType the node type of the described object
@@ -236,7 +236,7 @@ public class XMLInfo {
     public void setNodeType(short nodeType) {
         this.nodeType = nodeType;
     } //-- setNodeType
-    
+
     /**
      * Sets the XML Schema type for this XMLInfo
      * @param xsType the XML Schema type
@@ -244,7 +244,7 @@ public class XMLInfo {
     public void setSchemaType(XSType xsType) {
         this.xsType = xsType;
     } //-- setSchemaType
-    
+
     /**
      * Sets whether or not the XML object must appear at least once
      * @param required the flag indicating whether or not this XML
@@ -253,5 +253,5 @@ public class XMLInfo {
     public void setRequired(boolean required) {
         this.required = required;
     } //-- setRequired
-    
+
 } //-- XMLInfo
