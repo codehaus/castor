4d2165d5e6ff7395532ca84ac725a0a4f2ada07f
kvisco <kvisco@b24b0d9a-6811-0410-802a-946fa971d308>
2003-03-13 09:06:06 +0000

    added InternalXMLClassDescriptor for use within the Marshaller and Unmarshaller. Also added hasFieldsAtLocation() method for support of using xpath locations

--- trunk/castor/src/main/org/exolab/castor/xml/MarshalFramework.java
+++ trunk/castor/src/main/org/exolab/castor/xml/MarshalFramework.java
@@ -45,9 +45,13 @@
 
 package org.exolab.castor.xml;
 
+import org.exolab.castor.mapping.AccessMode;
+import org.exolab.castor.mapping.ClassDescriptor;
 import org.exolab.castor.mapping.CollectionHandler;
+import org.exolab.castor.mapping.FieldDescriptor;
 import org.exolab.castor.mapping.MappingException;
 import org.exolab.castor.mapping.loader.CollectionHandlers;
+import org.exolab.castor.mapping.ValidityException;
 
 import java.util.Vector;
 
@@ -152,6 +156,45 @@ abstract class MarshalFramework {
     } //-- isPrimitive
 
     /**
+     * Returns true if any of the fields associated with the given
+     * XMLClassDescriptor are located at, or beneath, the given location.
+     *
+     * @param location the location to compare against
+     * @param classDesc the XMLClassDescriptor in which to check the field 
+     * locations
+     */
+    static final boolean hasFieldsAtLocation
+        (String location, XMLClassDescriptor classDesc)
+    {
+        //-- check elements
+        XMLFieldDescriptor[] descriptors = classDesc.getElementDescriptors();
+        for (int i = 0; i < descriptors.length; i++) {
+            if (descriptors[i] == null) continue;
+            String tmpLocation = descriptors[i].getLocationPath();
+            if ((tmpLocation != null) && (tmpLocation.startsWith(location)))
+                return true;
+        }
+        
+        //-- check attributes
+        descriptors = classDesc.getAttributeDescriptors();
+        for (int i = 0; i < descriptors.length; i++) {
+            if (descriptors[i] == null) continue;
+            String tmpLocation = descriptors[i].getLocationPath();
+            if ((tmpLocation != null) && (tmpLocation.startsWith(location)))
+                return true;
+        }
+        
+        //-- check content/text
+        XMLFieldDescriptor content = classDesc.getContentDescriptor();
+        if (content != null) {
+            String tmpLocation = content.getLocationPath();
+            if ((tmpLocation != null) && (tmpLocation.startsWith(location)))
+                return true;
+        }
+        return false;
+    } //-- hasFieldsAtLocation
+    
+    /**
      * Returns true if the given classes are both the same
      * primitive or primitive wrapper class. For exmaple, if 
      * class "a" is an int (Integer.TYPE) and class "b" is 
@@ -206,7 +249,9 @@ abstract class MarshalFramework {
      * descriptor which match the given name and namespace.
      * @returns an array of InheritanceMatch.
      */
-    public static InheritanceMatch[] searchInheritance(String name, String namespace, XMLClassDescriptor classDesc, ClassDescriptorResolver cdResolver) {
+    public static InheritanceMatch[] searchInheritance
+        (String name, String namespace, XMLClassDescriptor classDesc, ClassDescriptorResolver cdResolver) 
+    {
 
 
         //-- A little required logic for finding Not-Yet-Loaded
@@ -278,6 +323,237 @@ abstract class MarshalFramework {
             inheritedClassDesc = classDesc;
         }
     }
+
+
+
+    /**
+     * An internal implementation of XMLClassDescriptor used by
+     * the Unmarshaller and Marshaller...
+     */
+    class InternalXMLClassDescriptor implements XMLClassDescriptor {
+
+        private XMLClassDescriptor _classDesc = null;
+        
+        /**
+         * Cached arrays
+         */
+        private XMLFieldDescriptor[] _attributes = null;
+        private XMLFieldDescriptor[] _elements   = null;
+        private FieldDescriptor[]    _fields     = null;
+        private XMLFieldDescriptor[] _nested     = null;
+        
+        /**
+         * Creates a new InternalXMLClassDescriptor for the given
+         * XMLClassDescriptor
+         */
+        protected InternalXMLClassDescriptor(XMLClassDescriptor classDesc)
+        {
+            if (classDesc == null) {
+                String err = "The argument 'classDesc' must not be null.";
+                throw new IllegalArgumentException(err);
+            }
+            
+            //-- prevent wrapping another InternalXMLClassDescriptor,
+            while (classDesc instanceof InternalXMLClassDescriptor) {
+                classDesc = ((InternalXMLClassDescriptor)classDesc).getClassDescriptor();
+            }
+            _classDesc = classDesc;
+        }
+        
+        /**
+         * Returns the XMLClassDescriptor that this InternalXMLClassDescriptor
+         * wraps.
+         *
+         * @return the XMLClassDescriptor
+         */
+        public XMLClassDescriptor getClassDescriptor() {
+            return _classDesc;
+        } //-- getClassDescriptor
+        
+        
+        /**
+         * Returns the set of XMLFieldDescriptors for all members
+         * that should be marshalled as XML attributes. This
+         * includes namespace nodes.
+         *
+         * @return an array of XMLFieldDescriptors for all members
+         * that should be marshalled as XML attributes.
+         */
+        public XMLFieldDescriptor[]  getAttributeDescriptors() {
+            if (_attributes == null) {
+                _attributes = _classDesc.getAttributeDescriptors();
+            }
+            return _attributes;
+        } //-- getAttributeDescriptors
+
+        /**
+         * Returns the XMLFieldDescriptor for the member
+         * that should be marshalled as text content.
+         *
+         * @return the XMLFieldDescriptor for the member
+         * that should be marshalled as text content.
+         */
+        public XMLFieldDescriptor getContentDescriptor() {
+            return _classDesc.getContentDescriptor();
+        } //-- getContentDescriptor
+
+
+        /**
+         * Returns the XML field descriptor matching the given
+         * xml name and nodeType. If NodeType is null, then
+         * either an AttributeDescriptor, or ElementDescriptor
+         * may be returned. Null is returned if no matching
+         * descriptor is available.
+         *
+         * @param name the xml name to match against
+         * @param nodeType, the NodeType to match against, or null if
+         * the node type is not known.
+         * @return the matching descriptor, or null if no matching
+         * descriptor is available.
+         *
+         */
+        public XMLFieldDescriptor getFieldDescriptor
+            (String name, NodeType nodeType) 
+        {
+            return _classDesc.getFieldDescriptor(name, nodeType);
+        } //-- getFieldDescriptor
+
+        /**
+         * Returns the set of XMLFieldDescriptors for all members
+         * that should be marshalled as XML elements.
+         *
+         * @return an array of XMLFieldDescriptors for all members
+         * that should be marshalled as XML elements.
+         */
+        public XMLFieldDescriptor[]  getElementDescriptors() {
+            if (_elements == null) {
+                _elements = _classDesc.getElementDescriptors();
+            }
+            return _elements;
+        } //-- getElementDescriptors
+
+        /**
+         * @return the namespace prefix to use when marshalling as XML.
+         */
+        public String getNameSpacePrefix() {
+            return _classDesc.getNameSpacePrefix();
+        } //-- getNameSpacePrefix
+
+        /**
+         * @return the namespace URI used when marshalling and unmarshalling as XML.
+         */
+        public String getNameSpaceURI() {
+            return _classDesc.getNameSpaceURI();
+        } //-- getNameSpaceURI
+
+        /**
+         * Returns a specific validator for the class described by
+         * this ClassDescriptor. A null value may be returned
+         * if no specific validator exists.
+         *
+         * @return the type validator for the class described by this
+         * ClassDescriptor.
+         */
+        public TypeValidator getValidator() {
+            return _classDesc.getValidator();
+        } //-- getValidator
+
+        /**
+         * Returns the XML Name for the Class being described.
+         *
+         * @return the XML name.
+         */
+        public String getXMLName() {
+            return _classDesc.getXMLName();
+        } //-- getXMLName
+
+        /**
+         * Returns true if the wrapped ClassDescriptor was created
+         * by introspection.
+         * 
+         * @return true if the wrapped ClassDescriptor was created
+         * by introspection.
+         */
+        public boolean introspected() {
+            return Introspector.introspected(_classDesc);
+        } //-- introspected
+        
+        /**
+         * <p>Returns true if the given object represented by this XMLClassDescriptor
+         * can accept a member whose name is given.
+         * An XMLClassDescriptor can accept a field if it contains a descriptor that matches
+         * the given name and if the given object can hold this field (i.e a value is not already set for
+         * this field).
+         * Different reasons can change the acceptance criteria, this is the reason why each implementation
+         * of XMLClassDescriptor must define these reasons.
+         *
+         * @param fieldName the name of the field to check
+         * @param object the object represented by this XMLCLassDescriptor
+         * @return true if the given object represented by this XMLClassDescriptor
+         * can accept a member whose name is given.
+         */
+        public boolean canAccept(String fieldName, Object object) {
+            return _classDesc.canAccept(fieldName, object);
+        } //-- canAccept
+
+
+        //-------------------------------------/
+        //- Implementation of ClassDescriptor -/
+        //-------------------------------------/
+        
+        /**
+         * Returns the Java class represented by this descriptor.
+         *
+         * @return The Java class
+         */
+        public Class getJavaClass() {
+            return _classDesc.getJavaClass();
+        } //-- getJavaClass
+
+
+        /**
+         * Returns a list of fields represented by this descriptor.
+         *
+         * @return A list of fields
+         */
+        public FieldDescriptor[] getFields() {
+            if (_fields == null) {
+                _fields = _classDesc.getFields();
+            }
+            return _fields;
+        } //-- getFields
+
+
+        /**
+         * Returns the class descriptor of the class extended by this class.
+         *
+         * @return The extended class descriptor
+         */
+        public ClassDescriptor getExtends() {
+            return _classDesc.getExtends();
+        } //-- getExtends
+
+
+        /**
+         * Returns the identity field, null if this class has no identity.
+         *
+         * @return The identity field
+         */
+        public FieldDescriptor getIdentity() {
+            return _classDesc.getIdentity();
+        } //-- getIdentity
+
+
+        /**
+         * Returns the access mode specified for this class.
+         *
+         * @return The access mode
+         */
+        public AccessMode getAccessMode() {
+            return _classDesc.getAccessMode();
+        } //-- getAccessMode
+        
+    } //-- InternalXMLClassDescriptor
 
 
 } //-- MarshalFramework
