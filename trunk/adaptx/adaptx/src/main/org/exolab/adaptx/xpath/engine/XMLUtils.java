b0e3b091df189475627722d3ed38dd69e8b9c9cc
kvisco <kvisco@b24b0d9a-6811-0410-802a-946fa971d308>
2003-05-08 22:43:02 +0000

    Fixed header information and author information...which has been incorrect for quite some time.

--- trunk/adaptx/adaptx/src/main/org/exolab/adaptx/xpath/engine/XMLUtils.java
+++ trunk/adaptx/adaptx/src/main/org/exolab/adaptx/xpath/engine/XMLUtils.java
@@ -1,20 +1,33 @@
-/**
- * Copyright (C) 2000, Intalio Inc.
+/*
+ * (C) Copyright Keith Visco 1999  All rights reserved.
  *
- * The program(s) herein may be used and/or copied only with the
- * written permission of Intalio Inc. or in accordance with the terms
- * and conditions stipulated in the agreement/contract under which the
- * program(s) have been supplied.
+ * The contents of this file are released under an Open Source 
+ * Definition (OSD) compliant license; you may not use this file 
+ * execpt in compliance with the license. Please see license.txt, 
+ * distributed with this file. You may also obtain a copy of the
+ * license at http://www.kvisco.com/xslp/license.txt
+ *
+ * The program is provided "as is" without any warranty express or
+ * implied, including the warranty of non-infringement and the implied
+ * warranties of merchantibility and fitness for a particular purpose.
+ * The Copyright owner will not be liable for any damages suffered by
+ * you as a result of using the Program. In no event will the Copyright
+ * owner be liable for any special, indirect or consequential damages or
+ * lost profits even if the Copyright owner has been advised of the
+ * possibility of their occurrence.
  *
  * $Id$
  */
-
 
 package org.exolab.adaptx.xpath.engine;
 
 
 /**
- * @author <a href="arkin@intalio.com">Assaf Arkin</a>
+ * A utility class for handling whitespace and namespaces.
+ *
+ * The code in this class was adapted from XSL:P.
+ * 
+ * @author <a href="kvisco@intalio.com">Keith Visco</a>
  * @version $Revision$
  */
 public final class XMLUtils
