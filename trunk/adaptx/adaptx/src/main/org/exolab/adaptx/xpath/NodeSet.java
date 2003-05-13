c140396348cbc3fa912b2fa9d30db56efa69fa8a
kvisco <kvisco@b24b0d9a-6811-0410-802a-946fa971d308>
2003-05-13 08:01:12 +0000

    updated header

--- trunk/adaptx/adaptx/src/main/org/exolab/adaptx/xpath/NodeSet.java
+++ trunk/adaptx/adaptx/src/main/org/exolab/adaptx/xpath/NodeSet.java
@@ -5,7 +5,7 @@
  * Definition (OSD) compliant license; you may not use this file 
  * execpt in compliance with the license. Please see license.txt, 
  * distributed with this file. You may also obtain a copy of the
- * license at http://www.clc-marketing.com/xslp/license.txt
+ * license at http://www.kvisco.com/xslp/license.txt
  *
  * The program is provided "as is" without any warranty express or
  * implied, including the warranty of non-infringement and the implied
@@ -30,7 +30,7 @@ package org.exolab.adaptx.xpath;
  * a node-set is not thread-safe and must not be used by two XPath
  * expressions evaluating concurrently.
  *
- * @author <a href="mailto:kvisco@ziplink.net">Keith Visco</a>
+ * @author <a href="mailto:kvisco@intalio.com">Keith Visco</a>
  * @author <a href="mailto:arkin@intalio.com">Assaf Arkin</a>
  * @version $Revision$ $Date$
  */
