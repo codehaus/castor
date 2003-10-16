cd3b4e063b4dd721b50fe2835ce69fbe146d7a16
blandin <blandin@b24b0d9a-6811-0410-802a-946fa971d308>
2003-10-16 22:52:56 +0000

    forward namespace mapping processing to SAX2ANY

--- trunk/castor/src/main/org/exolab/castor/xml/Unmarshaller.java
+++ trunk/castor/src/main/org/exolab/castor/xml/Unmarshaller.java
@@ -38,7 +38,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Copyright 1999-2002 (C) Intalio, Inc. All Rights Reserved.
+ * Copyright 1999-2003 (C) Intalio, Inc. All Rights Reserved.
  *
  * $Id$
  */
