0c5d8d05b865ac7919731af38e2214e374edcbc2
snyder <snyder@b24b0d9a-6811-0410-802a-946fa971d308>
2004-01-22 05:02:34 +0000

    Updated to allow Ant to handle the CLASSPATH within the build descriptor and to make use of Ant 1.5.

--- trunk/castor/build.sh
+++ trunk/castor/build.sh
@@ -14,8 +14,6 @@ fi
 
 JAVA=$JAVA_HOME/bin/java
 
-CLASSPATH=`echo lib/*.jar | tr ' ' ':'`
-CLASSPATH=lib/ant_1.3.jar:build/classes/:$CLASSPATH:$JAVA_HOME/lib/tools.jar
-
+CLASSPATH=lib/ant_1.5.jar:lib/xerces-J_1.4.0.jar:build/classes/:$CLASSPATH:$JAVA_HOME/lib/tools.jar
 
 $JAVA -classpath $CLASSPATH -Dant.home=lib org.apache.tools.ant.Main "$@" -buildfile src/build.xml
