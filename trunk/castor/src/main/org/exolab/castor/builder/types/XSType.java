8d456b14b68d1c408db150532e1939ed51bb8739
kvisco <kvisco@b24b0d9a-6811-0410-802a-946fa971d308>
2004-02-21 08:09:16 +0000

    Revert to version 1.2   (see bug 1541 for details)

--- trunk/castor/src/main/org/exolab/castor/builder/types/XSType.java
+++ trunk/castor/src/main/org/exolab/castor/builder/types/XSType.java
@@ -344,9 +344,7 @@ public abstract class XSType {
 
 	/**
 	 * Returns a list of Facets from the simpleType
-	 *(duplicate facets due to extension are filtered out).
-     * This rule does not apply for the pattern facet.
-     * 
+	 *	(duplicate facets due to extension are filtered out)
      * @param simpletype the Simpletype we want the facets for
      * @return Unique list of facets from the simple type
 	 */
@@ -354,16 +352,11 @@ public abstract class XSType {
 	{
 		Hashtable hashTable = new Hashtable();
         Enumeration enum = simpleType.getFacets();
-		int counter = 0;
-        while (enum.hasMoreElements()) {
+		while (enum.hasMoreElements()) {
 
             Facet facet = (Facet)enum.nextElement();
             String name = facet.getName();
-			if (name.equals("pattern")) {
-                name = name + counter;
-                counter++;        
-            }
-            hashTable.put(name, facet);
+			hashTable.put(name, facet);
 		}
 		return hashTable.elements();
 	}
