c1c1ce1e0bb9c2af10ec3e8e6d975d0c624610b7
wguttmann <wguttmann@b24b0d9a-6811-0410-802a-946fa971d308>
2004-06-08 11:04:52 +0000

    Restored revision 1.3.

--- trunk/castor/src/main/org/exolab/castor/jdo/drivers/SequenceKeyGenerator.java
+++ trunk/castor/src/main/org/exolab/castor/jdo/drivers/SequenceKeyGenerator.java
@@ -80,11 +80,7 @@ public final class SequenceKeyGenerator implements KeyGenerator
      * The <a href="http://jakarta.apache.org/commons/logging/">Jakarta
      * Commons Logging</a> instance used for all logging.
      */
-    private static Log _log = LogFactory.getFactory().getInstance(SequenceKeyGenerator.class);
-    
-    private DefaultIdentityValue identityValue = null;
-    private AbstractType type = null;
-    
+    private static Log _log = LogFactory.getFactory().getInstance (SequenceKeyGenerator.class);
     
     protected final PersistenceFactory _factory;
 
@@ -104,96 +100,56 @@ public final class SequenceKeyGenerator implements KeyGenerator
     private int _increment;
 
 
-    /**
-     * Indicates whether a trigger is present.
-     */
     private boolean _triggerPresent;
 
 
     /**
      * Initialize the SEQUENCE key generator.
-     * @param factory A PersistenceFactory instance.
-     * @param params Properties.
-     * @param sqlType A SQL type identifier.
-     * @throws MappingException Problem initializing this key generator.
      */
-    public SequenceKeyGenerator( PersistenceFactory factory, Properties params, int sqlType )
-	throws MappingException
+    public SequenceKeyGenerator( PersistenceFactory factory,
+            Properties params, int sqlType )
+            throws MappingException
     {
         boolean returning;
 
         _factoryName = factory.getFactoryName();
-        _factory = factory;
         returning = "true".equals( params.getProperty("returning") );
         _triggerPresent = "true".equals( params.getProperty("trigger","false") );
 
-        _seqName = params.getProperty("sequence", "{0}_seq");
-        
-        if ( (! _factoryName.equals( "oracle" )) &&  
-        		(! _factoryName.equals( "postgresql")) &&
-				(! _factoryName.equals( "interbase" )) &&
-				(! _factoryName.equals( "sapdb" )) &&
-				(! _factoryName.equals( "db2" )) ) {
-        	throw new MappingException( Messages.format( "mapping.keyGenNotCompatible",
+
+        if ( ! _factoryName.equals( "oracle" ) && ! _factoryName.equals( "postgresql" ) &&
+                ! _factoryName.equals( "interbase" ) && ! _factoryName.equals( "sapdb" ) &&
+                ! _factoryName.equals( "db2" ) ) {
+            throw new MappingException( Messages.format( "mapping.keyGenNotCompatible",
                                         getClass().getName(), _factoryName ) );
         }
-        if ((! _factoryName.equals( "oracle" )) && 
-        		(returning) ) {
+        if ( ! _factoryName.equals( "oracle" ) && returning ) {
             throw new MappingException( Messages.format( "mapping.keyGenParamNotCompat",
                                         "returning=\"true\"", getClass().getName(), _factoryName ) );
         }
+        _factory = factory;
+        _seqName = params.getProperty("sequence", "{0}_seq");
 
-        _style = (( _factoryName.equals( "postgresql" )) || 
-        		(_factoryName.equals("interbase"))  || 
-				(_factoryName.equals("db2"))
+        _style = ( _factoryName.equals( "postgresql" ) || _factoryName.equals("interbase")  || _factoryName.equals("db2")
                 ? BEFORE_INSERT : ( returning  ? DURING_INSERT : AFTER_INSERT) );
-        
         if (_triggerPresent && !returning) {
             _style = AFTER_INSERT;
         }
-        
         if (_triggerPresent && _style==BEFORE_INSERT)
             throw new MappingException( Messages.format( "mapping.keyGenParamNotCompat",
                                         "trigger=\"true\"", getClass().getName(), _factoryName ) );
 
         _sqlType = sqlType;
-        
-        if ((sqlType != Types.INTEGER) 
-		&& (sqlType != Types.NUMERIC) 
-		&& (sqlType != Types.DECIMAL) 
-		&& (sqlType != Types.BIGINT))
-        	throw new MappingException( Messages.format( "mapping.keyGenSQLType",
-                                        getClass().getName(), new Integer(sqlType)));
-        
-        
-        // etablish increment to use;if not specified, use default value of 1.
+        if (sqlType != Types.INTEGER && sqlType != Types.NUMERIC && sqlType != Types.DECIMAL && sqlType != Types.BIGINT)
+            throw new MappingException( Messages.format( "mapping.keyGenSQLType",
+                                        getClass().getName(), new Integer( sqlType ) ) );
         try {
-            _increment = Integer.parseInt(params.getProperty("increment", "1"));
+            _increment = Integer.parseInt(params.getProperty("increment","1"));
         } catch (NumberFormatException nfe) {
             _increment = 1;
         }
-        
-        initIdentityValue(sqlType);
-        initType(_factoryName);
-        
     }
 
-
-    /**
-     * @param conn An open connection within the given transaction
-     * @param tableName The table name
-     * @param primaryKeyName The primary key name
-     * @param props A temporary replacement for Principal object
-     * @return A new key
-     * @throws PersistenceException An error occured talking to persistent
-     *  storage
-     */
-    public Object generateKey( Connection conn, String tableName, String primaryKeyName,
-            Properties props )
-    throws PersistenceException
-    {
-        return type.getValue( conn, tableName, primaryKeyName, props);
-    }
 
     /**
      * @param conn An open connection within the given transaction
@@ -204,7 +160,7 @@ public final class SequenceKeyGenerator implements KeyGenerator
      * @throws PersistenceException An error occured talking to persistent
      *  storage
      */
-    public Object generateKeyOld( Connection conn, String tableName, String primKeyName,
+    public Object generateKey( Connection conn, String tableName, String primKeyName,
             Properties props )
             throws PersistenceException
     {
@@ -360,160 +316,4 @@ public final class SequenceKeyGenerator implements KeyGenerator
         return true;
     }
 
-    private void initIdentityValue(int sqlType) {
-        if (sqlType == Types.INTEGER) {
-            identityValue = new IntegerIdenityValue();
-        } else if (sqlType == Types.BIGINT) {
-            identityValue = new LongIdenityValue();
-        } else {
-            identityValue = new DefaultIdentityValue();
-        }
-    }
-    
-    private void initType(String fName) {
-        if (fName.equals("interbase")) {
-            type = new InterbaseType();
-        } else if (fName.equals("db2")) {
-            type = new DB2Type();
-        } else {
-            type = new DefaultType();
-        }
-    }
-    
-    
-    private class DefaultIdentityValue {
-        Object getValue(int value) {
-            return new BigDecimal(value);
-        }
-    }
-    
-    private class IntegerIdenityValue extends DefaultIdentityValue {
-        Object getValue(int value) {
-            return new Integer(value);
-        }
-    }
-    
-    private class LongIdenityValue extends DefaultIdentityValue {
-        Object getValue(int value) {
-            return new Long(value);
-        }
-    }
- 
-    private abstract class AbstractType {
-        abstract Object getValue(Connection conn, String sequenceName, String primaryKeyName, Properties props) 
-		throws PersistenceException;
-
-        Object getValue(PreparedStatement stmt) throws PersistenceException, SQLException {
-            ResultSet rs = stmt.executeQuery();
-            if (rs.next()) {
-                return identityValue.getValue(rs.getInt(1));
-            } else {
-                throw new PersistenceException(Messages.message("persist.keyGenFailed"));
-            }
-        }
-
-        Object getValue(String sql, Connection conn) throws PersistenceException {
-            PreparedStatement stmt = null;
-            try {
-                stmt = conn.prepareStatement(sql);
-                return getValue(stmt);
-            } catch (SQLException e) {
-                throw new PersistenceException(Messages.format("persist.keyGenSQL", e.toString()));
-            } finally {
-                if (stmt != null) {
-                    try {
-                        stmt.close();
-                    } catch (SQLException ex) {
-                        ex.printStackTrace();
-                    }
-                }
-            }
-        }
-    }
-    
-    private class DefaultType extends AbstractType {
-        
-        Object getValue(Connection conn, String tableName, String primaryKeyName, Properties props) 
-		throws PersistenceException 
-        {
-            StringBuffer buf = new StringBuffer();
-            
-            String sequenceName = getSequenceName(_seqName, tableName, primaryKeyName);
-            String table = _factory.quoteName(tableName);
-            
-            if ( _style == BEFORE_INSERT) {
-                buf.append ("SELECT nextval('").append(sequenceName).append("')");
-            } else if (_triggerPresent && _factoryName.equals( "postgresql" )) {
-                Object insStmt = props.get("insertStatement");
-                Class psqlStmtClass;
-                Method getInsertedOID;
-                int insertedOID;
-                PreparedStatement stmt;
-                try {
-					psqlStmtClass = Class.forName("org.postgresql.Statement");
-                    getInsertedOID = psqlStmtClass.getMethod ("getInsertedOID", null);
-                    insertedOID = ((Integer) getInsertedOID.invoke(insStmt, null)).intValue();
-                    stmt = conn.prepareStatement("SELECT " + _factory.quoteName( primaryKeyName ) +
-                            " FROM " + table + " WHERE OID=?");
-                    stmt.setInt(1, insertedOID);
-                    return getValue (stmt);
-                } 
-                catch (Exception e) {
-                    _log.warn (Messages.format("persist.keyGenSQL", e.toString()));
-                    throw new PersistenceException( Messages.format(
-                            "persist.keyGenSQL", e.toString() ) );
-				}
-
-            } else {
-                buf.append ("SELECT ").append(_factory.quoteName(sequenceName + ".currval")).append(" FROM " + table);
-            }
-            
-            return getValue(buf.toString(), conn);
-        }
-        
-        
-    }
-    
-    private class DB2Type extends AbstractType {
-        Object getValue(Connection conn, String tableName, String primaryKeyName, Properties props) 
-		throws PersistenceException 
-        {
-            String sequenceName = getSequenceName(_seqName, tableName, primaryKeyName);
-            StringBuffer buf = new StringBuffer();
-            buf.append ("SELECT nextval FOR ").append(sequenceName).append(" FROM SYSIBM.SYSDUMMY1");
-            return getValue(buf.toString(), conn);
-        }
-    }
-
-    private class InterbaseType extends AbstractType {
-        
-        Object getValue(Connection conn, String tableName, String primaryKeyName, Properties props) 
-		throws PersistenceException 
-        {
-            // interbase only does before_insert, and does it its own way
-            String sequenceName = getSequenceName(_seqName, tableName, primaryKeyName);
-            StringBuffer buf = new StringBuffer();
-        	buf.append("SELECT gen_id(")
-			.append(sequenceName)
-			.append(",")
-			.append(_increment)
-			.append(") FROM rdb$database");
-            return getValue(buf.toString(), conn);
-        }
-    }
-    
-    /**
-     * Helper method to derive complete name of the sequence.
-     * @param seqName Sequence name
-     * @param tableName Table name.
-     * @param primaryKeyName Name of the primary key.
-     * @return A complete sequence name.
-     */
-    private String getSequenceName (String seqName, String tableName, String primaryKeyName) {
-    	return MessageFormat.format(seqName, new String[] {tableName, primaryKeyName});    
-    }
-    
-    
-    
-    
 }
