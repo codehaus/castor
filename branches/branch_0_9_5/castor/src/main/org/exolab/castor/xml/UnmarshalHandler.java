090a8c5d04564ccd2f6b58b1f7d6b967799eac73
kvisco <kvisco@b24b0d9a-6811-0410-802a-946fa971d308>
2004-09-10 18:25:39 +0000

    A Xerces 2.x patch to ignore #startPrefixMapping calls with "xmlns" as the prefix

--- branches/branch_0_9_5/castor/src/main/org/exolab/castor/xml/UnmarshalHandler.java
+++ branches/branch_0_9_5/castor/src/main/org/exolab/castor/xml/UnmarshalHandler.java
@@ -2193,6 +2193,9 @@ public final class UnmarshalHandler extends MarshalFramework
         {
             return;
         }
+        else if (XMLNS.equals(prefix)) {
+            return;
+        }
         //-- end Xerces 2.x bug
         
         //-- Forward the call to SAX2ANY 
