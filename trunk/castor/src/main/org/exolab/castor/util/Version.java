a3ac9b19da486b9a6b5765a71117a6b2b3d36cbb
bsnyder <bsnyder@b24b0d9a-6811-0410-802a-946fa971d308>
2005-02-09 05:36:04 +0000

    Changed the VERSION for the release.

--- trunk/castor/src/main/org/exolab/castor/util/Version.java
+++ trunk/castor/src/main/org/exolab/castor/util/Version.java
@@ -67,7 +67,7 @@ public final class Version {
     /**
      * The version number
      */
-    public static final String VERSION = "cvs.0.9.6";
+    public static final String VERSION = "0.9.6";
         
     /**
      * The version date.
