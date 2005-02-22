51611462b6d1e1360e384c11e441481073f81a56
kvisco <kvisco@b24b0d9a-6811-0410-802a-946fa971d308>
2005-02-22 07:28:13 +0000

    updated to point to the new test locations

--- trunk/castor/CTFRun.bat
+++ trunk/castor/CTFRun.bat
@@ -9,12 +9,23 @@ rmdir /q /s .\build\tests\output>nul
 echo %CP%
 
 if "%1"=="" goto noargs
+if "%1"=="regression" goto regression
+if "%1"=="master" goto master
+
 %JAVA% -classpath %CP% org.exolab.castor.tests.framework.CastorTestSuiteRunner %1 %2 %3  %4 %5 %6 %7
 
 goto end
+
 :noargs
-echo using default arguments [-text -verbose src/tests/MasterTestSuite -printStack]
-%JAVA% -classpath %CP% org.exolab.castor.tests.framework.CastorTestSuiteRunner -text -verbose src/tests/MasterTestSuite -printStack
+:master
+echo using default arguments [-text -verbose src/tests/xml/MasterTestSuite]
+%JAVA% -classpath %CP% org.exolab.castor.tests.framework.CastorTestSuiteRunner -text -verbose src/tests/xml/MasterTestSuite
+
+goto end
+
+:regression
+echo using default arguments [-text -verbose src/tests/xml/RegressionTestSuite]
+%JAVA% -classpath %CP% org.exolab.castor.tests.framework.CastorTestSuiteRunner -text -verbose src/tests/xml/RegressionTestSuite
 
 :end
 set CLASSPATH=%OLDCP%
