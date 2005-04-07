1996bc4c07b2b0bc9961d42a4fca74ee899e4e11
rjoachim <rjoachim@b24b0d9a-6811-0410-802a-946fa971d308>
2005-04-07 20:52:20 +0000

    Changes to template for submitting bug reports

--- trunk/castor/src/bugs/jdo/template/create.sql
+++ trunk/castor/src/bugs/jdo/template/create.sql
@@ -3,6 +3,7 @@ create table entity1 (
   id        int not null,
   name      varchar(200) not null
 );
+
 -- create unique index entity1_pk on entity1 ( id );
 -- grant all privileges on entity1 to test;
 
