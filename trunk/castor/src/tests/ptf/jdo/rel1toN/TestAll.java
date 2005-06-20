d7ebe4e26a65f1135a1a2d6a2ad078ece798af41
rjoachim <rjoachim@b24b0d9a-6811-0410-802a-946fa971d308>
2005-06-20 00:33:54 +0000

    Fix small problem with ptf.jdo.TestAll and ptf.jdo.rel1toN.TestAll

--- trunk/castor/src/tests/ptf/jdo/rel1toN/TestAll.java
+++ trunk/castor/src/tests/ptf/jdo/rel1toN/TestAll.java
@@ -5,13 +5,19 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Run all tests of the ptf.jdo package.
+ * Run all tests of the ptf.jdo.rel1toN package.
  */
 public class TestAll extends TestCase {
     public static Test suite() throws Exception {
-        TestSuite suite = new TestSuite("All ptf.jdo performance tests");
+        TestSuite suite = new TestSuite("All ptf.jdo.rel1toN performance tests");
 
-        suite.addTest(ptf.jdo.rel1toN.TestAll.suite());
+        suite.addTest(TestCreate.suite());
+        suite.addTest(TestLoadBiNto1.suite());
+        suite.addTest(TestLoadUniNto1.suite());
+        suite.addTest(TestLoadBi1toN.suite());
+        suite.addTest(TestLoadLazy1toN.suite());
+        suite.addTest(TestLoadUni1toN.suite());
+        suite.addTest(TestRemove.suite());
 
         return suite;
     }
