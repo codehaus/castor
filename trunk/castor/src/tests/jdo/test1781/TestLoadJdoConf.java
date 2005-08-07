4a59c461514a37a811cae83cc7e46875a12ea1cf
rjoachim <rjoachim@b24b0d9a-6811-0410-802a-946fa971d308>
2005-08-07 09:55:02 +0000

    Created a test case for configuring JDO DataSource using a Properties collection (CASTOR-943)

--- trunk/castor/src/tests/jdo/test1781/TestLoadJdoConf.java
+++ trunk/castor/src/tests/jdo/test1781/TestLoadJdoConf.java
@@ -2,6 +2,8 @@ package jdo.test1781;
 
 import junit.framework.TestCase;
 
+import java.util.Properties;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -9,17 +11,11 @@ import org.exolab.castor.jdo.Database;
 import org.exolab.castor.jdo.JDOManager;
 import org.exolab.castor.jdo.OQLQuery;
 import org.exolab.castor.jdo.QueryResults;
+import org.exolab.castor.jdo.conf.JdoConf;
 import org.exolab.castor.jdo.util.JDOConfFactory;
 
 public final class TestLoadJdoConf extends TestCase {
     private static final Log LOG = LogFactory.getLog(TestLoadJdoConf.class);;
-    
-    public static void main(final String[] args) throws Exception {
-        TestLoadJdoConf test = new TestLoadJdoConf();
-        test.setUp();
-        test.testQueryEntityOne();
-        test.tearDown();
-    }
     
     public TestLoadJdoConf() {
         super();
@@ -37,15 +33,11 @@ public final class TestLoadJdoConf extends TestCase {
         super.tearDown();
     }
     
-    /**
-     * Test method.
-     * @throws Exception For any exception thrown.
-     */
-    public void testQueryEntityOne() throws Exception {
-        String DATABASE = "test";
+    public void testJdoDriverConf() throws Exception {
+        String DATABASE = "test-drv";
         String ENGINE = "mysql";
         String DRIVER = "org.gjt.mm.mysql.Driver";
-        String CONNECT = "jdbc:mysql://localhost/bugTemplate";
+        String CONNECT = "jdbc:mysql://localhost/test";
         String USERNAME = "test";
         String PASSWORD = "test";
         String MAPPING = "test1781-mapping.xml";
@@ -62,11 +54,49 @@ public final class TestLoadJdoConf extends TestCase {
         org.exolab.castor.jdo.conf.Database dbConf =
             JDOConfFactory.createDatabase(DATABASE, ENGINE, driverConf, mappingConf);
         
+        // test configuration
+        loadEntity(DATABASE, JDOConfFactory.createJdoConf(dbConf));
+    }
+    
+    public void testJdoDatasourceConf() throws Exception {
+        String DATABASE = "test-ds";
+        String ENGINE = "mysql";
+        String DATASOURCE = "org.apache.commons.dbcp.BasicDataSource";
+        String DRIVER = "org.gjt.mm.mysql.Driver";
+        String CONNECT = "jdbc:mysql://localhost/test";
+        String USERNAME = "test";
+        String PASSWORD = "test";
+        String MAPPING = "test1781-mapping.xml";
+        
+        Properties props = new Properties();
+        props.put("driver-class-name", DRIVER);
+        props.put("url", CONNECT);
+        props.put("username", USERNAME);
+        props.put("password", PASSWORD);
+        props.put("max-active", "10");
+        
+        // create driver configuration
+        org.exolab.castor.jdo.conf.DataSource datasourceConf =
+            JDOConfFactory.createDataSource(DATASOURCE, props);
+        
+        // create mapping configuration
+        org.exolab.castor.jdo.conf.Mapping mappingConf =
+            JDOConfFactory.createMapping(getClass().getResource(MAPPING).toString());
+
+        // create database configuration
+        org.exolab.castor.jdo.conf.Database dbConf =
+            JDOConfFactory.createDatabase(DATABASE, ENGINE, datasourceConf, mappingConf);
+        
+        // test configuration
+        loadEntity(DATABASE, JDOConfFactory.createJdoConf(dbConf));
+    }
+    
+    private void loadEntity(final String dbname, final JdoConf conf) throws Exception {
         // create and load jdo configuration
-        JDOManager.loadConfiguration(JDOConfFactory.createJdoConf(dbConf));
+        JDOManager.loadConfiguration(conf);
         
         // create jdo instance
-        JDOManager jdo = JDOManager.createInstance(DATABASE);
+        JDOManager jdo = JDOManager.createInstance(dbname);
 
         // create database instance and execute transaction
         Database db = jdo.getDatabase();
