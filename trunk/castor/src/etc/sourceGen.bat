fc36d72dd434756bdd300c4f23ef48bd0a684e4f
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2005-08-22 18:43:54 +0000

    Fixed problem with argument passing.

--- trunk/castor/src/etc/sourceGen.bat
+++ trunk/castor/src/etc/sourceGen.bat
@@ -6,7 +6,7 @@ for %%i in (lib\*.jar) do call cp.bat %%i
 set cp=%cp%;.
 
 
-%JAVA% -classpath %CP% org.exolab.castor.builder.SourceGenerator %1 %2 %3 %4 %5 %6 %7 %8 %9
+%JAVA% -classpath %CP% org.exolab.castor.builder.SourceGenerator %*
 
 
 
