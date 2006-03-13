37e6976b09de6997aab0ff5ae7a55dbc3b5b4afa
bsnyder <bsnyder@b24b0d9a-6811-0410-802a-946fa971d308>
2006-03-13 00:14:50 +0000

    Changed relative paths to absolute so the script works from outside the bin directory.

--- trunk/castor/bin/build.sh
+++ trunk/castor/bin/build.sh
@@ -14,11 +14,17 @@ fi
 
 JAVA=$JAVA_HOME/bin/java
 
-CLASSPATH=../lib/ant-1.6.jar:../lib/ant-1.6-launcher.jar:../lib/xerces-J_1.4.0.jar:../build/classes/:$CLASSPATH:$JAVA_HOME/lib/tools.jar:../lib/commons-logging.jar
+DIRNAME=`dirname $0`
+CASTOR_HOME=`cd $DIRNAME/..; pwd`
+LIB_D=$CASTOR_HOME/lib
+BUILD_D=$CASTOR_HOME/build
+SRC_D=$CASTOR_HOME/src
 
-for i in `ls ./../lib/*.jar`
+CLASSPATH=$LIB_D/ant-1.6.jar:$LIB_D/ant-1.6-launcher.jar:$LIB_D/xerces-J_1.4.0.jar:$BUILD_D/classes/:$CLASSPATH:$JAVA_HOME/lib/tools.jar:$LIB_D/commons-logging.jar
+
+for i in `ls $LIB_D/*.jar`
    do 
        CLASSPATH=$CLASSPATH:$i
    done
 
-$JAVA -classpath $CLASSPATH -Dant.home=lib org.apache.tools.ant.Main "$@" -buildfile ../src/build.xml
+$JAVA -classpath $CLASSPATH -Dant.home=lib org.apache.tools.ant.Main "$@" -buildfile $SRC_D/build.xml
