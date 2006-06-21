d53fd20987be6f04bfb06fe7516b4517d39e9c8a
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2006-06-21 10:11:04 +0000

    Moved to src/test/resources.

--- castor-spring/trunk/src/test/resource/create.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists product;
-create table product (
-	id int not null,
-	name varchar (20) not null,
-	primary key (id)
-);
-
-insert into product (id, name) values (1, 'product1');
