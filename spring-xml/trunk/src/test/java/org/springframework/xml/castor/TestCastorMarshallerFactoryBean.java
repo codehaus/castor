eeb47f1a5658d0ad4ceaa6bb46bf0979898f8bac
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2006-09-01 09:29:05 +0000

    Refcatored BaseSpringTestCase to allow definition of context resource file, as different test cases will be using individual Spring configurations.

--- spring-xml/trunk/src/test/java/org/springframework/xml/castor/TestCastorMarshallerFactoryBean.java
+++ spring-xml/trunk/src/test/java/org/springframework/xml/castor/TestCastorMarshallerFactoryBean.java
@@ -1,10 +1,6 @@
 package org.springframework.xml.castor;
 
-import java.io.StringWriter;
-import java.io.Writer;
 
-import org.exolab.castor.xml.Marshaller;
-import org.springframework.xml.entity.Product;
 
 /**
  * JUnit test case for Castor's Spring integration.
@@ -13,30 +9,9 @@ import org.springframework.xml.entity.Product;
  */
 public class TestCastorMarshallerFactoryBean extends BaseSpringTestCase {
 
-    public void testGetMarshaller() throws Exception {
-        Product product = new Product();
-        product.setId(1);
-        product.setName("blah");
-
-        Marshaller marshaller = (Marshaller) this.context.getBean("marshaller");
-        assertNotNull(marshaller);
-
-        Writer out = new StringWriter();
-        marshaller.setWriter(out);
-        marshaller.marshal(product);
-
-        System.out.println(out);
+    protected void setUp() throws Exception {
+        super.setUp();
     }
-
-    public void testGetMultipleMarshallers() throws Exception {
-        Marshaller firstMarshaller = (Marshaller) this.context.getBean("marshaller");
-        assertNotNull("Created Marshaller was null", firstMarshaller);
-
-        Marshaller secondMarshaller = (Marshaller) this.context.getBean("marshaller");
-        assertNotNull("Created Marshaller was null", secondMarshaller);
-
-        assertNotSame("Created Marshaller are same object, but must not", firstMarshaller, secondMarshaller);
-        assertSame("Created Marshallers have different Resolvers, but must have same", firstMarshaller.getResolver(), secondMarshaller
-                .getResolver());
-    }
+    
+    
 }
