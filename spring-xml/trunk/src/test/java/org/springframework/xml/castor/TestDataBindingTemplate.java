ffeb793e539bc66af291d955764968857b59658c
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2006-09-01 11:08:41 +0000

    Added Javadocs.

--- spring-xml/trunk/src/test/java/org/springframework/xml/castor/TestDataBindingTemplate.java
+++ spring-xml/trunk/src/test/java/org/springframework/xml/castor/TestDataBindingTemplate.java
@@ -5,18 +5,33 @@ import java.io.StringWriter;
 import org.springframework.xml.entity.Product;
 import org.xml.sax.InputSource;
 
+/**
+ * JUnit test case for testing various contracts as offered by the 
+ *
+ */
 public class TestDataBindingTemplate extends BaseSpringTestCase {
     
+    /**
+     * @see org.springframework.xml.castor.BaseSpringTestCase#setUp()
+     */
     protected void setUp() throws Exception {
         contextResource = "app-config-template.xml";
         super.setUp();
     }
     
+    /**
+     * Tests creation of the DataBindingTemplate through Spring.
+     * @throws Exception Any exception, really ....
+     */
     public void testCreateTemplate() throws Exception {
         DataBindingOperations template = (DataBindingOperations) context.getBean("template");
         assertNotNull(template);
     }
 
+    /**
+     * Tests the marshal() methdo of DataBindingTemplate
+     * @throws Exception Any exception really ...
+     */
     public void testMarshalling() throws Exception {
         DataBindingOperations template = (DataBindingOperations) context.getBean("template");
         assertNotNull(template);
@@ -33,6 +48,10 @@ public class TestDataBindingTemplate extends BaseSpringTestCase {
         assertNotNull(output);
     }
 
+    /**
+     * Tests the unmarshal() methdo of DataBindingTemplate-
+     * @throws Exception Any exception really ...
+     */
     public void testUnmarshalling() throws Exception {
         DataBindingOperations template = (DataBindingOperations) context.getBean("template");
         assertNotNull(template);
