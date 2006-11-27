dd04bacf9644fc19beef14ee56b230c971a33645
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2006-11-27 22:30:20 +0000

    Initial stub code to showcase implementation strategy.

---
+++ castor-jaxb-2.0/trunk/src/test/java/org/castor/jaxb/UnmarshallerTest.java
@@ -0,0 +1,11 @@
+package org.castor.jaxb;
+
+import junit.framework.TestCase;
+
+public class UnmarshallerTest extends TestCase {
+
+    public void testUnmarshalInputSource() {
+        fail("Not yet implemented");
+    }
+
+}
