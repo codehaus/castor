89a30bb6f789048416de293596561765ad304c3a
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2006-11-29 22:12:20 +0000

    Implemented the unmarshal(InputSource) method, adding a few sample domain entities as well. Please note that the instantiaton of the Unmarshaller class is not according to the spec .. ;-).

--- castor-jaxb-2.0/trunk/src/test/java/org/castor/jaxb/UnmarshallerTest.java
+++ castor-jaxb-2.0/trunk/src/test/java/org/castor/jaxb/UnmarshallerTest.java
@@ -1,11 +1,20 @@
 package org.castor.jaxb;
 
+import java.io.File;
+
+import javax.xml.bind.JAXBException;
+
+import org.xml.sax.InputSource;
+
 import junit.framework.TestCase;
 
 public class UnmarshallerTest extends TestCase {
 
-    public void testUnmarshalInputSource() {
-        fail("Not yet implemented");
+    public void testUnmarshalInputSource() throws JAXBException {
+        Unmarshaller unmarshaller = new Unmarshaller(org.castor.test.Test.class);
+        InputSource inputSource = new InputSource(getClass().getClassLoader().getResource("sample.xml").toExternalForm());
+        Object object = unmarshaller.unmarshal(inputSource);
+        assertNotNull(object);
     }
 
 }
