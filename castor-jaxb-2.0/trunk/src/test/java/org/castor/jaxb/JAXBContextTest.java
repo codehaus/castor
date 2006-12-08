6219e744fc80e2c246f893c5563a523f30045a04
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2006-12-08 17:56:39 +0000

    Started adding code to JAXBContext.java, and added additonal test cases for various methods of JAXBContext.

--- castor-jaxb-2.0/trunk/src/test/java/org/castor/jaxb/JAXBContextTest.java
+++ castor-jaxb-2.0/trunk/src/test/java/org/castor/jaxb/JAXBContextTest.java
@@ -1,11 +1,42 @@
 package org.castor.jaxb;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
 import junit.framework.TestCase;
+
+import org.castor.test.Test;
+import org.xml.sax.InputSource;
 
 public class JAXBContextTest extends TestCase {
 
-    public void testCreateUnmarshaller() {
-        fail("Not yet implemented");
+    public void testCreateJAXBContext() throws JAXBException {
+        JAXBContext context = org.castor.jaxb.JAXBContext.newInstance(Test.class);
+        assertNotNull(context);
     }
+    
+    public void testCreateJAXBContextForMOreThanOneClass() throws JAXBException {
+        JAXBContext context = org.castor.jaxb.JAXBContext.newInstance(new Class[] { Test.class });
+        assertNotNull(context);
+    }
+    
+    public void testCreateUnmarshaller() throws JAXBException {
+        JAXBContext context = org.castor.jaxb.JAXBContext.newInstance(Test.class);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        assertNotNull(unmarshaller);
+    }
+
+    
+    public void testCreateAndUseUnmarshaller() throws JAXBException {
+        JAXBContext context = new org.castor.jaxb.JAXBContext(Test.class);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        assertNotNull(unmarshaller);
+        
+        InputSource inputSource = new InputSource(getClass().getClassLoader().getResource("sample.xml").toExternalForm());
+        Object object = unmarshaller.unmarshal(inputSource);
+        assertNotNull(object);
+    }
+
 
 }
