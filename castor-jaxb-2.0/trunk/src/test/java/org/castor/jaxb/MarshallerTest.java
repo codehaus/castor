106da03d92026c85ce6c83295b20964ef0de05d6
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2006-12-27 17:34:19 +0000

    Added code to initially support schema based validation during marshalling, using JAXP 1.3 Validators.

--- castor-jaxb-2.0/trunk/src/test/java/org/castor/jaxb/MarshallerTest.java
+++ castor-jaxb-2.0/trunk/src/test/java/org/castor/jaxb/MarshallerTest.java
@@ -2,12 +2,17 @@ package org.castor.jaxb;
 
 import java.io.StringWriter;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBException;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 
 import junit.framework.TestCase;
 
 import org.castor.test.Child;
 import org.castor.test.Test;
+import org.castor.test.TestValidationHandler;
 
 public class MarshallerTest extends TestCase {
 
@@ -23,6 +28,85 @@ public class MarshallerTest extends TestCase {
         test.setChild(child);
         
         marshaller.marshal(test, out);
+        
+        System.out.println(out.toString());
+        
+        assertNotNull(out);
+        
+        String xml = out.toString();
+        assertNotNull(xml);
+        assertTrue(xml.length() > 0);
+    }
+
+    public void testMarshalTest2ToWriter() throws JAXBException {
+        
+        javax.xml.bind.JAXBContext context = JAXBContext.newInstance(org.castor.test2.Test.class);
+        javax.xml.bind.Marshaller marshaller = context.createMarshaller();
+        
+        StringWriter out = new StringWriter();
+        
+        org.castor.test2.Test test = new org.castor.test2.Test();
+        org.castor.test2.Child child = new org.castor.test2.Child();
+        child.setName("werner");
+        test.setChild(child);
+        
+        marshaller.marshal(test, out);
+        
+        System.out.println(out.toString());
+        
+        assertNotNull(out);
+        
+        String xml = out.toString();
+        assertNotNull(xml);
+        assertTrue(xml.length() > 0);
+    }
+
+    public void testMarshalToWriterWithValidation() throws Exception {
+        javax.xml.bind.JAXBContext context = JAXBContext.newInstance(Test.class);
+        javax.xml.bind.Marshaller marshaller = context.createMarshaller();
+        
+        StringWriter out = new StringWriter();
+        
+        Test test = new Test();
+        Child child = new Child();
+        child.setName("werner");
+        test.setChild(child);
+        
+        
+        SchemaFactory factory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
+        Schema schema = factory.newSchema(new StreamSource(getClass().getClassLoader().getResourceAsStream("test.xsd")));
+        marshaller.setSchema(schema);
+        marshaller.setEventHandler(new TestValidationHandler());
+        marshaller.marshal(test, out);
+        
+        System.out.println(out.toString());
+        
+        assertNotNull(out);
+        
+        String xml = out.toString();
+        assertNotNull(xml);
+        assertTrue(xml.length() > 0);
+    }
+
+    public void testMarshalToWriterAlternateWithValidation() throws Exception {
+        javax.xml.bind.JAXBContext context = JAXBContext.newInstance(Test.class);
+        javax.xml.bind.Marshaller marshaller = context.createMarshaller();
+        
+        StringWriter out = new StringWriter();
+        
+        org.castor.test2.Test test = new org.castor.test2.Test();
+        org.castor.test2.Child child = new org.castor.test2.Child();
+        child.setName("werner");
+        test.setChild(child);
+        
+        
+        SchemaFactory factory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
+        Schema schema = factory.newSchema(new StreamSource(getClass().getClassLoader().getResourceAsStream("testa.xsd")));
+        marshaller.setSchema(schema);
+        marshaller.setEventHandler(new TestValidationHandler());
+        marshaller.marshal(test, out);
+        
+        System.out.println(out.toString());
         
         assertNotNull(out);
         
---
+++ castor-jaxb-2.0/trunk/src/test/java/org/castor/test2/Test.java
@@ -0,0 +1,14 @@
+package org.castor.test2;
+
+public class Test {
+
+    private Child child;
+
+    public Child getChild() {
+        return child;
+    }
+
+    public void setChild(Child child) {
+        this.child = child;
+    }
+}
