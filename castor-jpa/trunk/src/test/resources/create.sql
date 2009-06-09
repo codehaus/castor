cecc0c97248566b2cf438e37fb63dd878a353f06
lukaslang <lukaslang@b24b0d9a-6811-0410-802a-946fa971d308>
2009-06-09 18:38:28 +0000

    Added initial JPA testcases with Spring setup.

--- castor-jpa/trunk/src/test/resources/create.sql
+++ castor-jpa/trunk/src/test/resources/create.sql
@@ -1,8 +1,9 @@
-drop table if exists product;
+-- drop table product;
+
 create table product (
-	id int not null,
-	name varchar (20) not null,
-	primary key (id)
+    id int not null,
+    name varchar (20) not null,
+    primary key (id)
 );
 
 insert into product (id, name) values (1, 'product1');
---
+++ castor-jpa/trunk/src/test/resources/org/castor/jpa/functional/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE book (
+    isbn INT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL
+);
