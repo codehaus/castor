23347972d291356bed932feb4be4b3fb6e27299a
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2011-08-05 16:06:28 +0000

    Moved property names to their own compilation units (to shorten length of classes).

---
+++ castor-jaxb-2.0/trunk/src/main/java/org/castor/jaxb/reflection/info/JaxbClassNatureProperties.java
@@ -0,0 +1,42 @@
+package org.castor.jaxb.reflection.info;
+
+public interface JaxbClassNatureProperties {
+    /** . */
+    String ENUM_CLASS = "ENUM_CLASS";
+    /** . */
+    String SEE_ALSO_CLASSES = "SEE_ALSO_CLASSES";
+    /** . */
+    String TRANSIENT = "TRANSIENT";
+    /** . */
+    String XML_ACCESS_ORDER = "XML_ACCESS_ORDER";
+    /** . */
+    String XML_ACCESS_TYPE = "XML_ACCESS_TYPE";
+    /** . */
+    String ROOT_ELEMENT_NAMESPACE = "ROOT_ELEMENT_NAMESPACE";
+    /** . */
+    String ROOT_ELEMENT_NAME = "ROOT_ELEMENT_NAME";
+    /** . */
+    String TYPE_FACTORY_METHOD = "TYPE_FACTORY_METHOD";
+    /** . */
+    String TYPE_FACTORY_CLASS = "TYPE_FACTORY_CLASS";
+    /** . */
+    String TYPE_NAMESPACE = "TYPE_NAMESPACE";
+    /** . */
+    String TYPE_PROPERTIES = "TYPE_PROPERTIES";
+    /** . */
+    String TYPE_NAME = "TYPE_NAME";
+    /** . */
+    String HAS_XML_TYPE = "HAS_XML_TYPE";
+    /** . */
+    String HAS_XML_ROOT_ELEMENT = "HAS_XML_ROOT_ELEMENT";
+    /** . */
+    String HAS_XML_TRANSIENT = "HAS_XML_TRANSIENT";
+    /** . */
+    String HAS_XML_SEE_ALSO = "HAS_XML_SEE_ALSO";
+    /** . */
+    String HAS_XML_ACCESSOR_TYPE = "HAS_XML_ACCESSOR_TYPE";
+    /** . */
+    String HAS_XML_ACCESSOR_ORDER = "HAS_XML_ACCESSOR_ORDER";
+    /** . */
+    String HAS_XML_ENUM = "HAS_XML_ENUM";
+}
